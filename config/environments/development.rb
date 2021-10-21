7f4fa98734e5ea0d7b846de3fc07b2434c68a4d9
Joe Masilotti <joe@masilotti.com>
2021-10-21 13:58:14 -0700

    Test CI via GitHub Actions (#4)
    * Prefer double quotes
    
    * Check in schema
    
    * Clean up output in bin/setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
+  # since you don"t have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Do not eager load code on boot.
