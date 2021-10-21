0f876e6f5465a421ee5f967d8a8cb10811df23fb
Rob Zolkos <rob@zolkos.com>
2021-10-21 17:35:05 -0400

    Fix test selector

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,10 +6,10 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "#show home page" do
     get root_path
     assert_response :success
-    assert_text "Rails Devs"
+    assert_select "h1", text: "Rails Devs"
 
     get home_path
     assert_response :success
-    assert_text "Rails Devs"
+    assert_select "h1", text: "Rails Devs"
   end
 end
