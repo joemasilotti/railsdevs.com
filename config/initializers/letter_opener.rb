a2b52c0f170f960ae8430d31f55a8a8aae5a5582
Joe Masilotti <joe@masilotti.com>
2021-11-02 12:53:58 -0700

    Remove Letter Opener configuration

--- config/initializers/letter_opener.rb
+++
@@ -1,9 +0,0 @@
-LetterOpener.configure do |config|
-  # To overrider the location for message storage.
-  # Default value is `tmp/letter_opener`
-  config.location = Rails.root.join("tmp", "my_mails")
-
-  # To render only the message body, without any metadata or extra containers or styling.
-  # Default value is `:default` that renders styled message with showing useful metadata.
-  config.message_template = :light
-end
