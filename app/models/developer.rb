c80ecac1c3ee0061176d3a248f762878f87fd7d7
Joe Masilotti <joe@masilotti.com>
2021-11-02 12:41:35 -0700

    Max file size validation

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,11 +1,12 @@
 class Developer < ApplicationRecord
+  belongs_to :user
+  has_one_attached :avatar
+
   validates :name, presence: true
   validates :email, presence: true
   validates :available_on, presence: true
   validates :hero, presence: true
   validates :bio, presence: true
-  validates :avatar, content_type: ["image/png", "image/jpg", "image/jpeg"]
-
-  belongs_to :user
-  has_one_attached :avatar
+  validates :avatar, content_type: ["image/png", "image/jpg", "image/jpeg"],
+    max_file_size: 2.megabytes
 end
---
+++ app/validators/max_file_size_validator.rb
@@ -0,0 +1,13 @@
+class MaxFileSizeValidator < ActiveModel::EachValidator
+  include ActionView::Helpers::NumberHelper
+
+  def validate_each(record, attribute, value)
+    return unless value.attached?
+
+    max_file_size = options[:with]
+    if record.send(attribute).blob.byte_size > max_file_size
+      size = number_to_human_size(max_file_size)
+      record.errors.add(attribute, :max_file_size_invalid, size: size)
+    end
+  end
+end
