0e5aaa04393872039b79c9566a468cd9cd5f9a98
Lewis Sparlin <hello@lewismsparlin.com>
2021-11-05 21:31:20 -0500

    Add component html.erb files to purge aware list

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
   purge: [
     "./app/views/**/*.html.erb",
     "./app/components/**/*.rb",
+    "./app/components/**/*.html.erb",
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js"
   ],
