65a08196a44807da9912964db21c619a31b21569
Ahmad khatab <dev.ahmad.khattab@gmail.com>
2021-11-07 01:55:30 +0300

    force redirect to request.referer when logging out

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,4 +2,8 @@ class Users::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     developer_path resource
   end
+
+  def after_sign_out_path_for(resource)
+    request.referer || root_path
+  end
 end
