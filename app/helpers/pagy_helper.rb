eec3d05116938c457ef54ac51472ca4c5c17da54
Ahmad khatab <dev.ahmad.khattab@gmail.com>
2021-11-06 17:18:54 +0300

    extracted pagy-links element into it's own helper method

--- app/helpers/pagy_helper.rb
+++ app/helpers/pagy_helper.rb
@@ -1,3 +1,7 @@
 module PagyHelper
   include Pagy::Frontend
+
+  def pagy_links(**args, &block)
+    tag.div(class: "invisible opacity-0 w-0 h-0", id: "pagy-links", **args, data: {pagination_target: "links"}, &block)
+  end
 end
