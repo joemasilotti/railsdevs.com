70c63fabd07f4556e5f91815957e60002ad0dc14
Ahmad khatab <dev.ahmad.khattab@gmail.com>
2021-11-07 01:40:30 +0300

    redirect to profile when user signs in successfully

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,5 @@
+class Users::SessionsController < Devise::SessionsController
+  def after_sign_in_path_for(resource)
+    developer_path resource
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {sessions: "users/sessions"}
 
   resources :developers, except: %i[destroy]
 
