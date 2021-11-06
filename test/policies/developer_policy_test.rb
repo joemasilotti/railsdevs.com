1f5c8f310bf9c13b708411103e1e90830cf0caa0
Jacob Daddario <jake.daddario@gmail.com>
2021-11-06 09:35:20 -0500

    feat: Validate that user doesn't already have dev (#34)
    * feat: Validate that user doesn't already have dev
    
    * developerprofile -> developer profile
    
    * Clean up fixture names
    
    * Use association methods to build developer profile
    
    Co-authored-by: Joe Masilotti <joe@masilotti.com>

--- test/policies/developer_policy_test.rb
+++ test/policies/developer_policy_test.rb
@@ -1,19 +1,31 @@
 require "test_helper"
 
 class DeveloperPolicyTest < ActiveSupport::TestCase
-  test "can create and update their own developerprofile" do
-    user = users(:one)
+  test "update their own developer profile" do
+    user = users(:with_profile_one)
     developer = developers(:one)
 
-    assert DeveloperPolicy.new(user, developer).create?
     assert DeveloperPolicy.new(user, developer).update?
   end
 
-  test "cannot create nor update another's developerprofile" do
-    user = users(:one)
+  test "cannot update another's developer profile" do
+    user = users(:with_profile_one)
     developer = developers(:two)
 
-    refute DeveloperPolicy.new(user, developer).create?
     refute DeveloperPolicy.new(user, developer).update?
+  end
+
+  test "can create developer profile if they do not already have one" do
+    user = users(:without_profile)
+    developer = user.developer
+
+    assert DeveloperPolicy.new(user, developer).create?
+  end
+
+  test "cannot create developer profile for self if they already have one" do
+    user = users(:with_profile_one)
+    developer = user.developer
+
+    refute DeveloperPolicy.new(user, developer).create?
   end
 end
