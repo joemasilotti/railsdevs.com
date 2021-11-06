251549e445c0c365f696636dfb147361bc1955fa
Joe Masilotti <joe@masilotti.com>
2021-11-06 08:03:43 -0700

    Code formatting

--- app/javascript/controllers/pagination_controller.js
+++ app/javascript/controllers/pagination_controller.js
@@ -1,12 +1,11 @@
-import { Controller } from "@hotwired/stimulus";
-import { get } from "@rails/request.js";
+import { Controller } from "@hotwired/stimulus"
+import { get } from "@rails/request.js"
 
 export default class extends Controller {
   static targets = ["entries", "links"]
 
   initialize() {
-    const options = { rootMargin: "200px" }
-
+    const options = {rootMargin: "200px"}
     this.intersectionObserver = new IntersectionObserver((entries) => this.processIntersectionEntries(entries), options)
   }
 
--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require "pagy"
 
 # Pagy initializer file (5.1.3)
