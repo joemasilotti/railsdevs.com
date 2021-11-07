bd5591a20674c7a67e90db6d7ec1f4b89299ea8f
Ahmad khatab <dev.ahmad.khattab@gmail.com>
2021-11-07 11:56:06 +0300

    standardrb --fix

--- test/integration/users/sessions_test.rb
+++ test/integration/users/sessions_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class Users::SessionsTest < ActionDispatch::IntegrationTest
   test "it redirects to developer profile if profile is present" do
-    post user_session_path user: { email: users(:with_profile_one).email, password: 'password'}
+    post user_session_path user: {email: users(:with_profile_one).email, password: "password"}
     assert_redirected_to developer_path developers(:one)
   end
 
   test "it redirects to root path when user does not have developer account" do
-    post user_session_path user: { email: users(:without_profile).email, password: 'password'}
+    post user_session_path user: {email: users(:without_profile).email, password: "password"}
     assert_redirected_to root_path
   end
 
@@ -20,7 +20,7 @@ class Users::SessionsTest < ActionDispatch::IntegrationTest
 
   test "it redirects to referrer when logging out if referer is set" do
     sign_in users(:with_profile_one)
-    delete destroy_user_session_path, headers: { "HTTP_REFERER":  developer_path(developers(:one)) }
+    delete destroy_user_session_path, headers: {HTTP_REFERER: developer_path(developers(:one))}
 
     assert_redirected_to developer_path developers(:one)
   end
