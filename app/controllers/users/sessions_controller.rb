9d792be9550ae14938039b58c573aea6b16043f4
Ahmad khatab <dev.ahmad.khattab@gmail.com>
2021-11-07 11:49:51 +0300

    add tests to verify code behavior

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,10 @@
 class Users::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
-    developer_path resource
+    if resource.developer.present?
+      developer_path resource.developer
+    else
+      root_path
+    end
   end
 
   def after_sign_out_path_for(resource)
