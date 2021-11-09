938826eecf1222b54af97b06e0b56fc459cb1c3e
Joe Masilotti <joe@masilotti.com>
2021-11-09 09:00:03 -0800

    Refactor meta generation and test helper

--- app/components/developer_open_graph_tags_component.rb
+++ app/components/developer_open_graph_tags_component.rb
@@ -2,4 +2,8 @@ class DeveloperOpenGraphTagsComponent < ViewComponent::Base
   def initialize(developer:)
     @developer = developer
   end
+
+  def twitter
+    "@#{@developer.twitter}" if @developer.twitter.present?
+  end
 end
--- test/components/developer_open_graph_meta_tags_component_test.rb
+++ test/components/developer_open_graph_meta_tags_component_test.rb
@@ -1,43 +1,47 @@
 require "test_helper"
 
 class DeveloperOpenGraphTagsComponentTest < ViewComponent::TestCase
-  def test_always_includes_basic_tags
-    developer = Developer.new(
-      id: 123,
-      hero: "hero text",
-      bio: "bio text"
-    )
+  test "always includes basic tags" do
+    developer = Developer.new(id: 123, hero: "hero text", bio: "bio text")
+
     render_inline(DeveloperOpenGraphTagsComponent.new(developer: developer))
 
-    assert_meta "[property='og:title'][content='hero text']"
-    assert_meta "[property='og:description'][content='bio text']"
-    assert_meta "[property='og:url'][content='http://test.host/developers/123']"
+    assert_meta property: "og:title", content: "hero text"
+    assert_meta property: "og:description", content: "bio text"
+    assert_meta property: "og:url", content: "http://test.host/developers/123"
   end
 
-  def test_excludes_special_tags_when_not_present
+  test "excludes special tags when not present" do
     developer = Developer.new(id: 123)
+
     render_inline(DeveloperOpenGraphTagsComponent.new(developer: developer))
 
-    assert_meta "[property='og:image']", count: 0
-    assert_meta "[name='twitter:site']", count: 0
+    assert_meta property: "og:image", count: 0
+    assert_meta property: "twitter:site", count: 0
   end
 
-  def test_includes_image_when_present
+  test "includes image when present" do
     developer = Developer.new(id: 123)
     developer.build_avatar_blob(filename: "avatar.jpg")
+
     render_inline(DeveloperOpenGraphTagsComponent.new(developer: developer))
 
-    assert_meta "[property='og:image'][content$='/avatar.jpg']"
+    assert_meta property: "og:image", content_end_with: "/avatar.jpg"
   end
 
-  def test_includes_twitter_when_present
+  test "includes twitter when present" do
     developer = Developer.new(id: 123, twitter: "me")
+
     render_inline(DeveloperOpenGraphTagsComponent.new(developer: developer))
 
-    assert_meta "[name='twitter:site'][content='@me']"
+    assert_meta property: "twitter:site", content: "@me"
   end
 
-  def assert_meta(attributes_selector, count: 1)
-    assert_selector "meta#{attributes_selector}", visible: false, count: count
+  def assert_meta(property:, content: nil, content_end_with: nil, count: 1)
+    selector = "meta[property='#{property}']"
+    selector += "[content='#{content}']" if content.present?
+    selector += "[content$='#{content}']" if content_end_with.present?
+
+    assert_selector selector, visible: false, count: count
   end
 end
