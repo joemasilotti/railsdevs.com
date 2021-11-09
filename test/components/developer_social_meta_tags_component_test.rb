73bbd1067c2ac8a102e3479353af6f119305cf09
Joe Masilotti <joe@masilotti.com>
2021-11-09 06:51:03 -0800

    standardize

--- test/components/developer_social_meta_tags_component_test.rb
+++ test/components/developer_social_meta_tags_component_test.rb
@@ -9,17 +9,17 @@ class DeveloperSocialMetaTagsComponentTest < ViewComponent::TestCase
     )
     render_inline(DeveloperSocialMetaTagsComponent.new(developer: developer))
 
-    assert_meta '[property="og:title"][content="hero text"]'
-    assert_meta '[property="og:description"][content="bio text"]'
-    assert_meta '[property="og:url"][content="http://test.host/developers/123"]'
+    assert_meta "[property='og:title'][content='hero text']"
+    assert_meta "[property='og:description'][content='bio text']"
+    assert_meta "[property='og:url'][content='http://test.host/developers/123']"
   end
 
   def test_excludes_special_tags_when_not_present
     developer = Developer.new(id: 123)
     render_inline(DeveloperSocialMetaTagsComponent.new(developer: developer))
 
-    assert_meta '[property="og:image"]', count: 0
-    assert_meta '[name="twitter:site"]', count: 0
+    assert_meta "[property='og:image']", count: 0
+    assert_meta "[name='twitter:site']", count: 0
   end
 
   def test_includes_image_when_present
@@ -27,14 +27,14 @@ class DeveloperSocialMetaTagsComponentTest < ViewComponent::TestCase
     developer.build_avatar_blob(filename: "avatar.jpg")
     render_inline(DeveloperSocialMetaTagsComponent.new(developer: developer))
 
-    assert_meta '[property="og:image"][content$="/avatar.jpg"]'
+    assert_meta "[property='og:image'][content$='/avatar.jpg']"
   end
 
   def test_includes_twitter_when_present
     developer = Developer.new(id: 123, twitter: "me")
     render_inline(DeveloperSocialMetaTagsComponent.new(developer: developer))
 
-    assert_meta '[name="twitter:site"][content="@me"]'
+    assert_meta "[name='twitter:site'][content='@me']"
   end
 
   def assert_meta(attributes_selector, count: 1)
