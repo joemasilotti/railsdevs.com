6c9f3dde496e18d8be52194c4fbe1d0c6b6c470a
Tonksthebear <jasonconig@me.com>
2021-11-10 20:26:40 -0800

    Replace pagination with turbo (#80)
    * fetch upstream
    
    * Change: Update toggle controller to have option perform toggle on load
    
    * Change: Add turbo frames to developer index in order to leverage turbo for infinite scroll
    Change: Pagy helper to use updated toggle controller for hiding when detecting javascript
    Remove: Unneccessary pagination controller due to leveraging turbo
    Remove: Developer index.turbo_stream
    
    * Remove: @rails/request.js from bundle as we are using turbo for pagination
    
    * Run `yarn install`
    
    * Center pagination (shown when JS is disabled)
    
    * Change: Match desired syntax style for index.html
    Change: Make toggle controller more powerful and allow setting of open state
    Change: Update header to use new syntax
    Change: Update header to > Stimulus 2.0 syntax for defining targets
    Change: Update pagy links to use newer toggler syntax
    
    * Change: Ignore first page developers in turbo stream since it is already rendered outside of frame
    
    * Change: Remove extra blank line
    
    Co-authored-by: Joe Masilotti <joe@masilotti.com>

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,13 +2,31 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["element"]
-  static classes = ["visibility"]
+  static classes = ["open", "close"]
+  static values = {
+    open: { type: Boolean, default: false }
+  }
+
+  openValueChanged() {
+    this.openValue ? this.toggleOpen() : this.toggleClose()
+  }
 
   toggle(event) {
-    event.preventDefault()
+    event && event.preventDefault()
+    this.openValue = !this.openValue
+  }
 
+  toggleOpen() {
     this.elementTargets.forEach(element => {
-      element.classList.toggle(this.visibilityClass)
+      this.hasCloseClass && element.classList.remove(this.closeClasses)
+      this.hasOpenClass && element.classList.add(this.openClasses)
+    })
+  }
+
+  toggleClose() {
+    this.elementTargets.forEach(element => {
+      this.hasOpenClass && element.classList.remove(this.openClasses)
+      this.hasCloseClass && element.classList.add(this.closeClasses)
     })
   }
 }
