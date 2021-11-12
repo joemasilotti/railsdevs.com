a089e3779805aecda17b7adb464962f4da5671eb
Tonksthebear <jasonconig@me.com>
2021-11-11 17:26:39 -0800

    standardrb changes

--- config/initializers/form_errors.rb
+++ config/initializers/form_errors.rb
@@ -1,5 +1,5 @@
 # Add error span to form fields with errors
-Rails.application.config.action_view.field_error_proc = proc do |html_tag, instance| 
+Rails.application.config.action_view.field_error_proc = proc do |html_tag, instance|
   if /<label/.match?(html_tag)
     errors = Array(instance.error_message).join(",")
     error_message = %(<span class="mt-2 text-sm font-medium text-red-600">&nbsp;#{errors}</span>).html_safe
