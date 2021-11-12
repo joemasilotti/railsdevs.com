aebc926a9099872d3e70a242dbb0babf089938d2
Tonksthebear <jasonconig@me.com>
2021-11-12 10:49:50 -0800

    Change: Change the initializer to use ActionView::Base Change: Change field error proc to remove text color styling from label and wrap label in element that styles all text red

--- config/initializers/form_errors.rb
+++ config/initializers/form_errors.rb
@@ -1,10 +1,14 @@
 # Add error span to form fields with errors
-Rails.application.config.action_view.field_error_proc = proc do |html_tag, instance|
+ActionView::Base.field_error_proc = proc do |html_tag, instance|
   if /<label/.match?(html_tag)
+    # Remove text color if it exists so that container color takes precedence
+    text_color_regex = %r{text-\w*-\d*[^\s"]}
+    text_color_free_html_tag = html_tag.gsub(text_color_regex, "")
+
     errors = Array(instance.error_message).join(",")
-    error_message = %(<span class="mt-2 text-sm font-medium text-red-600">&nbsp;#{errors}</span>).html_safe
-    closing_tag_index = html_tag.index("</label>")
-    html_tag.insert(closing_tag_index, error_message)
+    error_message = %(<span class="mt-2 text-sm font-medium">&nbsp;#{errors}</span>).html_safe
+    closing_tag_index = text_color_free_html_tag.index("</label>")
+    %(<div class="contents text-red-600">#{text_color_free_html_tag.insert(closing_tag_index, error_message)}</div>).html_safe
   else
     html_tag
   end
