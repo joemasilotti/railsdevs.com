ae2fddb0883e96037cd1bcc00252ace59484db97
Tonksthebear <jasonconig@me.com>
2021-11-15 11:22:35 -0800

    Change: Rename @spoofed_instance to @instance in test

--- test/models/field_error_tag_builder_test.rb
+++ test/models/field_error_tag_builder_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class FieldErrorTagBuilderTest < ActiveSupport::TestCase
   setup do
-    @spoofed_instance = Struct.new(:error_message).new(["fake error"])
+    @instance = Struct.new(:error_message).new(["fake error"])
   end
 
   test "replace only text color class" do
     html_tag = %(<label class="bg-gray-200 font-medium text-gray-200">Test Label</label>)
-    builder = FieldErrorTagBuilder.new(html_tag, @spoofed_instance)
+    builder = FieldErrorTagBuilder.new(html_tag, @instance)
 
     assert_equal html_tag.length - builder.color_free_tag.length, "text-gray-200".length
     assert_not builder.color_free_tag.include?("text-gray-200")
@@ -15,7 +15,7 @@ class FieldErrorTagBuilderTest < ActiveSupport::TestCase
 
   test "grabs correct closing tag index" do
     html_tag = %(<label class="bg-gray-200 font-medium">Test Label</label>)
-    builder = FieldErrorTagBuilder.new(html_tag, @spoofed_instance)
+    builder = FieldErrorTagBuilder.new(html_tag, @instance)
     actual_index = html_tag.index("</label")
 
     assert_equal builder.closing_tag_index, actual_index
@@ -23,8 +23,8 @@ class FieldErrorTagBuilderTest < ActiveSupport::TestCase
 
   test "inserts errors before closing tag" do
     html_tag = %(<label class="bg-gray-200 font-medium">Test Label</label>)
-    builder = FieldErrorTagBuilder.new(html_tag, @spoofed_instance)
+    builder = FieldErrorTagBuilder.new(html_tag, @instance)
 
-    assert builder.error_field.index(@spoofed_instance.error_message.first) < builder.error_field.index("</label>")
+    assert builder.error_field.index(@instance.error_message.first) < builder.error_field.index("</label>")
   end
 end
