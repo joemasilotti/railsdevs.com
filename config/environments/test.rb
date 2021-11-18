51d691f40b88a1bb26c286563f502d934c9b7885
Joe Masilotti <joe@masilotti.com>
2021-11-18 10:33:56 -0800

    Raise on missing translations in dev/test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.active_storage.service = :test
 
   # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
