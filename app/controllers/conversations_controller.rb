f5c758145d8b934c32fb37628403fea766c8cee5
Jacob Daddario <jake.daddario@gmail.com>
2021-11-18 20:25:40 -0600

    WIP SAVEPOINT

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,7 +1,10 @@
 class ConversationsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
   end
 
   def index
+    @conversations = policy_scope(Conversation)
   end
 end
