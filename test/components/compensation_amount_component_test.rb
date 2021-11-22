fb8d786e62e386fc0bb58c99a42f6699e5a2bd3b
Peter Berkenbosch <info@peterberkenbosch.nl>
2021-11-22 09:39:51 +0100

    Add tests for the amount and compensation components

--- test/components/compensation_amount_component_test.rb
+++ test/components/compensation_amount_component_test.rb
@@ -1,10 +1,14 @@
 require "test_helper"
 
 class CompensationAmountComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(CompensationAmountComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  test "doesn't render with no amount" do
+    render_inline CompensationAmountComponent.new(amount: nil, title: "Hourly")
+    assert_no_select "*"
+  end
+
+  test "renders with amount in USD and title" do
+    render_inline CompensationAmountComponent.new(amount: 150, title: "Hourly")
+    assert_text "Hourly"
+    assert_text "$150.00"
   end
 end
--- test/components/expected_compensation_component_test.rb
+++ test/components/expected_compensation_component_test.rb
@@ -1,10 +1,26 @@
 require "test_helper"
 
 class ExpectedCompensationComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(ExpectedCompensationComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  setup do
+    @developer = developers(:available)
+  end
+
+  test "renders nothing whith no expected compensation" do
+    render_inline(ExpectedCompensationComponent.new(@developer))
+    assert_no_select "*"
+  end
+
+  test "renders hourly rate if provided" do
+    @developer.expected_hourly_rate = 150
+    render_inline(ExpectedCompensationComponent.new(@developer))
+    assert_text "Hourly"
+    assert_text "$150.00"
+  end
+
+  test "renders yearly salary if provided" do
+    @developer.expected_salary = 250000
+    render_inline(ExpectedCompensationComponent.new(@developer))
+    assert_text "Yearly salary"
+    assert_text "$250,000.00"
   end
 end
