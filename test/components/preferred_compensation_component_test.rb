c530bb75f1879ed8f433160ef47faf75e1ac99d9
Peter Berkenbosch <info@peterberkenbosch.nl>
2021-11-26 00:36:45 +0100

    Rename test to proper component name

--- test/components/preferred_compensation_component_test.rb
+++ test/components/preferred_compensation_component_test.rb
@@ -1,26 +1,26 @@
 require "test_helper"
 
-class ExpectedCompensationComponentTest < ViewComponent::TestCase
+class PreferredCompensationComponentTest < ViewComponent::TestCase
   setup do
     @developer = developers(:available)
   end
 
   test "renders nothing whith no expected compensation" do
-    render_inline(ExpectedCompensationComponent.new(@developer))
+    render_inline(PreferredCompensationComponent.new(@developer))
     assert_no_select "*"
   end
 
   test "renders hourly rate if provided" do
     @developer.preferred_min_hourly_rate = 150
-    render_inline(ExpectedCompensationComponent.new(@developer))
+    render_inline(PreferredCompensationComponent.new(@developer))
     assert_text "Hourly"
-    assert_text "$150.00"
+    assert_text "$150"
   end
 
   test "renders yearly salary if provided" do
     @developer.preferred_min_salary = 250000
-    render_inline(ExpectedCompensationComponent.new(@developer))
+    render_inline(PreferredCompensationComponent.new(@developer))
     assert_text "Yearly salary"
-    assert_text "$250,000.00"
+    assert_text "$250K"
   end
 end
