da684c63029f2a329db634114291d237ea9fd936
Joe Masilotti <joe@masilotti.com>
2021-11-28 08:18:56 -0800

    Move migration after latest from main

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_171626) do
+ActiveRecord::Schema.define(version: 2021_12_68_083538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
