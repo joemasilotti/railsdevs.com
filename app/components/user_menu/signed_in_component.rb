988e89ba377419267aef07af0f323840ac7e4493
Joe Masilotti <joe@masilotti.com>
2021-11-29 14:55:10 -0800

    Split out business links from developer dropdown

--- app/components/user_menu/signed_in_component.rb
+++ app/components/user_menu/signed_in_component.rb
@@ -5,20 +5,6 @@ class UserMenu::SignedInComponent < ApplicationComponent
     @user = user
   end
 
-  def developer_link?
-    (developer? && !business?) | (!developer? && !business?) || (developer? && business?)
-  end
-
-  def business_link?
-    (business? && !developer?) || (developer? && business?)
-  end
-
-  private
-
-  def developer?
-    user.developer.present?
-  end
-
   def business?
     user.business.present?
   end
--- test/components/user_menu/signed_in_component_test.rb
+++ test/components/user_menu/signed_in_component_test.rb
@@ -4,31 +4,15 @@ class UserMenu::SignedInComponentTest < ViewComponent::TestCase
   include ActionDispatch::Routing::UrlFor
   include Rails.application.routes.url_helpers
 
-  test "no developer or business, links to developer" do
-    user = users(:without_profile)
-    render_inline UserMenu::SignedInComponent.new(user)
-    assert_selector "a[href='#{new_developer_path}']"
-    assert_no_selector "a[href='#{new_business_path}']"
-  end
-
-  test "developer only, links to it" do
-    user = users(:with_available_profile)
-    render_inline UserMenu::SignedInComponent.new(user)
-    assert_selector "a[href='#{new_developer_path}']"
-    assert_no_selector "a[href='#{new_business_path}']"
-  end
-
-  test "business only, links to it" do
+  test "links to business" do
     user = users(:with_business)
     render_inline UserMenu::SignedInComponent.new(user)
-    assert_no_selector "a[href='#{new_developer_path}']"
     assert_selector "a[href='#{new_business_path}']"
   end
 
-  test "both developer and business, links to both" do
-    user = users(:with_profile_and_business)
+  test "doesn't link to business" do
+    user = users(:with_available_profile)
     render_inline UserMenu::SignedInComponent.new(user)
-    assert_selector "a[href='#{new_developer_path}']"
-    assert_selector "a[href='#{new_business_path}']"
+    assert_no_selector "a[href='#{new_business_path}']"
   end
 end
