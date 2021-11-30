4f538b2e2c92a35e6c601833bf9f86099192bd9d
Joe Masilotti <joe@masilotti.com>
2021-11-30 12:43:10 -0800

    Revert "TEMP: Comment out migration to sync with prod"
    This reverts commit 6ff272108bbe84398202d81052b3541d8ce3970f.

--- db/migrate/20211128000000_add_preferred_compensation_fields_to_developers.rb
+++ db/migrate/20211128000000_add_preferred_compensation_fields_to_developers.rb
@@ -1,9 +1,8 @@
 class AddPreferredCompensationFieldsToDevelopers < ActiveRecord::Migration[7.0]
   def change
-    # TEMP: Comment out migration to sync with production version.
-    # add_column :developers, :preferred_min_hourly_rate, :integer
-    # add_column :developers, :preferred_max_hourly_rate, :integer
-    # add_column :developers, :preferred_min_salary, :integer
-    # add_column :developers, :preferred_max_salary, :integer
+    add_column :developers, :preferred_min_hourly_rate, :integer
+    add_column :developers, :preferred_max_hourly_rate, :integer
+    add_column :developers, :preferred_min_salary, :integer
+    add_column :developers, :preferred_max_salary, :integer
   end
 end
