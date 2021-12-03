9c27e18be0543fe655605228cb7935a3001cde69
Joe Masilotti <joe@masilotti.com>
2021-12-02 20:07:09 -0800

    Remove unused code

--- app/errors/application_error.rb
+++
@@ -1,7 +0,0 @@
-# app/errors/application_error.rb
-class ApplicationError < StandardError
-  # Look up translations via "errors.module_name.class_name".
-  def message
-    I18n.t("errors.#{self.class.name.underscore.tr("/", ".")}")
-  end
-end
