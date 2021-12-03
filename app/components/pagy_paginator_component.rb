66a87862ad1b1be6622f4b40fec23babfe9c1c15
Tonksthebear <jasonconig@me.com>
2021-12-02 22:18:40 -0800

    Change: Remove instance variables from view component views

--- app/components/pagy_paginator_component.rb
+++ app/components/pagy_paginator_component.rb
@@ -4,6 +4,8 @@ class PagyPaginatorComponent < ApplicationComponent
   include PagyHelper
   renders_one :loading_icon
 
+  attr_reader :id, :pagy, :url_array, :container_classes
+
   def initialize(id:, pagy:, url_array:, container_classes:, direction: :bottom)
     @id = id
     @pagy = pagy
