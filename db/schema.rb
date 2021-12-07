97d2fafaed24036dcfde159993170dd792d4943f
Joe Masilotti <joe@masilotti.com>
2021-12-07 09:37:42 -0800

    Add back in removed blank line

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_11_28_095707) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
