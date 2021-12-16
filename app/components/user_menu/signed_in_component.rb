1c01cfff817004818cf7b53bca95f3dafe0ead12
Jay Clark <jay@jayeclark.dev>
2021-12-16 15:31:18 -0400

    Basic framework for notification badge
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- app/components/user_menu/signed_in_component.rb
+++ app/components/user_menu/signed_in_component.rb
@@ -21,7 +21,24 @@ class UserMenu::SignedInComponent < ApplicationComponent
     (user.conversations.any? || business?) && Feature.enabled?(:messaging)
   end
 
+  def unread_notifications
+    user.notifications.reject { |n| n[:read_at] > 0 }
+  end
+
+  def no_unread_notifications?
+    !unread_notifications.any?
+  end
+
   def admin?
     user.admin?
+  end
+
+  def conditional_wrapper(condition = true, options = {}, &block)
+    options[:tag] ||= :div
+    if condition == true
+      concat content_tag(options[:tag], capture(&block), options.delete_if { |k, v| k == :tag })
+    else
+      concat capture(&block)
+    end
   end
 end
