2983b5e87de1e286474bf71bf1e017f87cbdd0d8
hungmi <hungmihungmi@gmail.com>
2021-12-24 18:45:18 +0800

    bundle exec standardrb --fix

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -25,20 +25,20 @@ SitemapGenerator::Sitemap.create do
   #     add article_path(article), :lastmod => article.updated_at
   #   end
 
-  add developers_path, changefreq: 'always', priority: 1, lastmod: Developer.order(id: :desc).first.try(:updated_at)
-  add about_path, changefreq: 'weekly', priority: 0.9
+  add developers_path, changefreq: "always", priority: 1, lastmod: Developer.order(id: :desc).first.try(:updated_at)
+  add about_path, changefreq: "weekly", priority: 0.9
 
   Developer.most_recently_added.find_each do |developer|
-    add developer_path(id: developer.id), changefreq: 'always', priority: 0.8, lastmod: developer.updated_at
+    add developer_path(id: developer.id), changefreq: "always", priority: 0.8, lastmod: developer.updated_at
   end
 
-  add new_user_session_path, changefreq: 'weekly', priority: 0.7
-  add new_user_registration_path, changefreq: 'weekly', priority: 0.7
-  add new_user_password_path, changefreq: 'monthly', priority: 0.7
-  add new_user_confirmation_path, changefreq: 'monthly', priority: 0.7
+  add new_user_session_path, changefreq: "weekly", priority: 0.7
+  add new_user_registration_path, changefreq: "weekly", priority: 0.7
+  add new_user_password_path, changefreq: "monthly", priority: 0.7
+  add new_user_confirmation_path, changefreq: "monthly", priority: 0.7
 
-  add new_role_path, changefreq: 'weekly', priority: 0.6
-  add new_developer_path, changefreq: 'weekly', priority: 0.6
-  add new_business_path, changefreq: 'weekly', priority: 0.6
-  add pricing_path, changefreq: 'weekly', priority: 0.5
+  add new_role_path, changefreq: "weekly", priority: 0.6
+  add new_developer_path, changefreq: "weekly", priority: 0.6
+  add new_business_path, changefreq: "weekly", priority: 0.6
+  add pricing_path, changefreq: "weekly", priority: 0.5
 end
