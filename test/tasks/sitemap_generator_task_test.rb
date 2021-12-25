c0c520b95dbacc6037a196b3c103da83c1dd2755
hungmi <hungmihungmi@gmail.com>
2021-12-25 17:08:58 +0800

    bin/check

--- test/tasks/sitemap_generator_task_test.rb
+++ test/tasks/sitemap_generator_task_test.rb
@@ -1,9 +1,9 @@
-require 'test_helper'
-require 'rake'
+require "test_helper"
+require "rake"
 Rails.application.load_tasks
 
 class SitemapGeneratorTaskTest < ActiveSupport::TestCase
-	test "sitemap is generated without any errors" do
-		Rake::Task["sitemap:refresh:no_ping"].invoke
-	end
+  test "sitemap is generated without any errors" do
+    Rake::Task["sitemap:refresh:no_ping"].invoke
+  end
 end
