4eb33cd5c7fbbd2764c5c0cf1e645fe8ec7a7ec1
hungmi <hungmihungmi@gmail.com>
2021-12-25 17:09:46 +0800

    Disable sitemap generator outputs

--- test/tasks/sitemap_generator_task_test.rb
+++ test/tasks/sitemap_generator_task_test.rb
@@ -4,6 +4,8 @@ Rails.application.load_tasks
 
 class SitemapGeneratorTaskTest < ActiveSupport::TestCase
   test "sitemap is generated without any errors" do
+    # disable sitemap generator outputs https://github.com/kjvarga/sitemap_generator/issues/332
+    Rake::FileUtilsExt.verbose(false)
     Rake::Task["sitemap:refresh:no_ping"].invoke
   end
 end
