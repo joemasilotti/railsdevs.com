0122f702deca1cc4727d08b6e39bd6a3c7e33d09
hungmi <hungmihungmi@gmail.com>
2021-12-25 16:51:02 +0800

    run the sitemap generator task in test to ensure it runs without errors

---
+++ test/tasks/sitemap_generator_task_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+require 'rake'
+Rails.application.load_tasks
+
+class SitemapGeneratorTaskTest < ActiveSupport::TestCase
+	test "sitemap is generated without any errors" do
+		Rake::Task["sitemap:refresh:no_ping"].invoke
+	end
+end
