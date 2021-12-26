620b867ac6013a7117d6b3b9ce52ce0278225da5
Jay Clark <jay@jayeclark.dev>
2021-12-26 11:33:04 -0400

    Update model and prep assets
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,12 @@ class Notification < ApplicationRecord
   include Noticed::Model
 
   belongs_to :recipient, polymorphic: true
+
+  def conversation
+    message.conversation_id
+  end
+
+  def message
+    params[:message]
+  end
 end
