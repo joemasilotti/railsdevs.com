623758ef5e193424a5822416fc1b7eb2b36c94a2
Joe Masilotti <joe@masilotti.com>
2021-12-27 14:25:42 -0500

    Clean up test comments and formatting

--- test/tasks/sitemap_generator_task_test.rb
+++ test/tasks/sitemap_generator_task_test.rb
@@ -1,11 +1,15 @@
 require "test_helper"
 require "rake"
-Rails.application.load_tasks
 
 class SitemapGeneratorTaskTest < ActiveSupport::TestCase
+  Rails.application.load_tasks
+
   test "sitemap is generated without any errors" do
-    # disable sitemap generator outputs https://github.com/kjvarga/sitemap_generator/issues/332
+    disable_sitemap_generator_output
+    Rake::Task["sitemap:create"].invoke
+  end
+
+  def disable_sitemap_generator_output
     Rake::FileUtilsExt.verbose(false)
-    Rake::Task["sitemap:refresh:no_ping"].invoke
   end
 end
