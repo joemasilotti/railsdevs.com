6c468d410954007a1ac5f73f9fdc909b6e8e4b80
Jacob Daddario <jake.daddario@gmail.com>
2021-12-28 19:25:13 -0600

    feat: Does not paywall content to content owner

--- app/components/paywall_component.rb
+++ app/components/paywall_component.rb
@@ -1,9 +1,24 @@
 class PaywallComponent < ApplicationComponent
-  def initialize(user:)
+  def initialize(user:, paywalled:)
     @user = user
+    @paywalled = paywalled
   end
+
+  def paywalled?
+    customer? || owner?
+  end
+
+  private
 
   def customer?
     @user.active_business_subscription?
+  end
+
+  def owner?
+    begin
+      @paywalled.user == @user
+    rescue
+      false
+    end
   end
 end
--- test/components/paywall_component_test.rb
+++ test/components/paywall_component_test.rb
@@ -3,15 +3,33 @@ require "test_helper"
 class PaywallComponentTest < ViewComponent::TestCase
   test "should restrict paywall content" do
     user = users(:with_business)
-    render_inline(PaywallComponent.new(user: user)) { "Test text" }
+    developer = developers(:available)
+    render_inline(PaywallComponent.new(user: user, paywalled: developer)) { "Test text" }
 
     assert_no_text "Test text"
   end
 
   test "should should show paywall content to customers" do
     user = users(:with_business_conversation)
-    render_inline(PaywallComponent.new(user: user)) { "Test text" }
+    developer = developers(:available)
+    render_inline(PaywallComponent.new(user: user, paywalled: developer)) { "Test text" }
 
     assert_text "Test text"
+  end
+
+  test "should show paywall content to the owner" do
+    user = users(:with_available_profile)
+    developer = developers(:available)
+    render_inline(PaywallComponent.new(user: user, paywalled: developer)) { "Test text" }
+
+    assert_text "Test text"
+  end
+
+  test "paywalls content when detecting ownership on bad target" do
+    user = users(:with_available_profile)
+    developer = "Bad input"
+    render_inline(PaywallComponent.new(user: user, paywalled: developer)) { "Test text" }
+
+    assert_no_text "Test text"
   end
 end
