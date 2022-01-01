e605836b1cf1eb7d45b7116dd7a801a72ecfb21e
Joe Masilotti <joe@masilotti.com>
2022-01-01 14:27:08 -0800

    Actually hide the notification bell in prod

--- app/components/user_menu/signed_in_component.rb
+++ app/components/user_menu/signed_in_component.rb
@@ -30,6 +30,6 @@ class UserMenu::SignedInComponent < ApplicationComponent
   end
 
   def unread_notifications?
-    user.message_notifications.unread&.any? && Feature.enabled?(:notifications)
+    user.message_notifications.unread&.any?
   end
 end
