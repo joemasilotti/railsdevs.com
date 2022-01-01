7a952c2f6bfcbadc2d4c019b3c1a083b015820bb
Joe Masilotti <joe@masilotti.com>
2022-01-01 13:52:43 -0800

    Feature flag notifications UI and mark_as_read!

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -9,10 +9,13 @@ class ConversationsController < ApplicationController
     @conversation = Conversation.find(params[:id])
     @message = Message.new
     authorize @conversation, policy_class: MessagingPolicy
-    @unread_notifications = current_user.conversation_notifications(@conversation)
-    if @unread_notifications.any?
-      @unread_notifications.each { |n| n.mark_as_read! }
-      redirect_to conversation_path(@conversation)
+
+    if Feature.enabled?(:notifications)
+      @unread_notifications = current_user.conversation_notifications(@conversation)
+      if @unread_notifications.any?
+        @unread_notifications.each { |n| n.mark_as_read! }
+        redirect_to conversation_path(@conversation)
+      end
     end
   end
 end
