41ce4269d0a746f8264f1de0f81e4418c37059f4
Jesper Christiansen <hi@jespr.com>
2022-01-02 13:58:36 -0800

    Introduce empty-state component (#202)

---
+++ test/components/empty_state_component_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class EmptyStateComponentTest < ViewComponent::TestCase
+  test "Renders the title, body and icon" do
+    title = "No notifications"
+    body = "Nothing to see"
+
+    render_inline(EmptyStateComponent.new(title: title, body: body, icon: "icons/brands/github.svg"))
+
+    assert_selector("h3", text: title)
+    assert_selector("p", text: body)
+    assert_selector "svg[title='GitHub logo']"
+  end
+end
