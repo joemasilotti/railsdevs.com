f6542ac518f174e1b4448b1cf5da0cb457780bca
Joe Masilotti <joe@masilotti.com>
2022-01-03 06:25:18 -0800

    Eager load developer role types (n+1 query) (#205)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def show
-    @developers = Developer.available.most_recently_added.with_attached_avatar.limit(10)
+    @developers = Developer
+      .includes(:role_type).with_attached_avatar
+      .available.most_recently_added
+      .limit(10)
   end
 end
