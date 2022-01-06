2b36caafe70aea46aef5f70cb126a9825f1513d4
Jacob Daddario <jake.daddario@gmail.com>
2022-01-05 21:22:26 -0600

    review: Completed review changes

--- test/components/developer_primary_action_component_test.rb
+++ test/components/developer_primary_action_component_test.rb
@@ -10,6 +10,11 @@ class DeveloperPrimaryActionComponentTest < ViewComponent::TestCase
 
     assert_text "Hire me"
     refute_text "Edit"
+
+    user = users(:with_business)
+    render_inline DeveloperPrimaryActionComponent.new(user: user, developer: @developer)
+
+    assert_text "Hire me"
   end
 
   test "should show edit to owner of profile" do
@@ -17,12 +22,5 @@ class DeveloperPrimaryActionComponentTest < ViewComponent::TestCase
     render_inline DeveloperPrimaryActionComponent.new(user: user, developer: @developer)
 
     assert_text "Edit"
-  end
-
-  test "should show message button to business account" do
-    user = users(:with_business)
-    render_inline DeveloperPrimaryActionComponent.new(user: user, developer: @developer)
-
-    assert_text "Hire me"
   end
 end
