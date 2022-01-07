3cc740646fea5fba4b21217ddc970a86aa999169
soc-man <okoth.sc@gmail.com>
2022-01-07 04:55:39 +0300

    add primary_time_zone validation on create

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -19,6 +19,7 @@ class Developer < ApplicationRecord
   validates :hero, presence: true
   validates :bio, presence: true
   validates :search_status, inclusion: { in: search_statuses.keys}, on: :create
+  validates :primary_time_zone, presence: true, on: :create
   validates :cover_image, content_type: ["image/png", "image/jpeg", "image/jpg"],
     max_file_size: 10.megabytes
   validates :preferred_max_hourly_rate, allow_nil: true, numericality: {greater_than_or_equal_to: :preferred_min_hourly_rate}, if: -> { preferred_min_hourly_rate.present? }
