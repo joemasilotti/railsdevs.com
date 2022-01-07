f39f1f32b804f7c755975626d5264718ee779014
Ignacio Mella <imellat@gmail.com>
2022-01-07 18:50:52 -0300

    refactor SubmitButtonComponent
    - Move the button element inside the component
    - Use tailwind pseudo classes to show or hide the disable_with elements
    - Conditional disable_with text for developer and business forms

--- test/components/submit_button_component_test.rb
+++ test/components/submit_button_component_test.rb
@@ -3,12 +3,12 @@ require "test_helper"
 class SubmitButtonComponentTest < ViewComponent::TestCase
   def test_component_renders_something_useful
     text = "Save"
-    disable_with = "Processing"
+    disable_with = "Updating..."
 
     render_inline(SubmitButtonComponent.new(text: text, disable_with: disable_with))
 
-    assert_selector("span.show-when-enabled", text: text)
-    assert_selector("span.show-when-disabled", text: disable_with)
-    assert_selector("svg.show-when-disabled")
+    assert_selector("span.show.group-disabled\\:hidden", text: text)
+    assert_selector("span.hidden.group-disabled\\:inline", text: disable_with)
+    assert_selector("svg.hidden.group-disabled\\:inline")
   end
 end
