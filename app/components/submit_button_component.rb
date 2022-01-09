6e87f5782d9577668bef83d62fa1bf53628c4586
Ignacio Mella <imellat@gmail.com>
2022-01-09 19:43:43 -0300

    make button_classes additive instead of destructive

--- app/components/submit_button_component.rb
+++ app/components/submit_button_component.rb
@@ -1,15 +1,9 @@
 class SubmitButtonComponent < ApplicationComponent
-  attr_reader :text, :disable_with, :button_clasess
+  attr_reader :text, :disable_with, :button_classes
 
   def initialize(text:, disable_with: nil, button_classes: nil)
     @text = text
     @disable_with = disable_with
-    @button_clasess = button_clasess || default_button_clasess
-  end
-
-  def default_button_clasess
-    "ml-3 inline-flex justify-center align-center py-2 px-4 border border-transparent"\
-    "shadow-sm text-sm font-medium rounded-md text-white bg-gray-600 cursor-pointer"\
-    "hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+    @button_classes = button_classes
   end
 end
--- test/components/submit_button_component_test.rb
+++ test/components/submit_button_component_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
 class SubmitButtonComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
+  test "should render enabled and disabled button states" do
     text = "Save"
     disable_with = "Updating..."
 
-    render_inline(SubmitButtonComponent.new(text: text, disable_with: disable_with))
+    render_inline(SubmitButtonComponent.new(text:, disable_with:))
 
-    assert_selector("span.show.group-disabled\\:hidden", text: text)
+    assert_selector("span.show.group-disabled\\:hidden", text:)
     assert_selector("span.hidden.group-disabled\\:inline", text: disable_with)
     assert_selector("svg.hidden.group-disabled\\:inline")
   end
