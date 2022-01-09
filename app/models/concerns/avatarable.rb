ca932a928b73c76e1cd80283b3001b2486328c97
soc-man <okoth.sc@gmail.com>
2022-01-09 04:46:34 +0300

    edit avatar validation

--- app/models/concerns/avatarable.rb
+++ app/models/concerns/avatarable.rb
@@ -5,6 +5,15 @@ module Avatarable
     has_one_attached :avatar
 
     validates :avatar, content_type: ["image/png", "image/jpeg", "image/jpg"],
-      max_file_size: 2.megabytes, presence: true, on: :create
+      max_file_size: 2.megabytes
+    validate :avatar_is_attached?, on: :create
+
+    private
+
+    def avatar_is_attached?
+      unless avatar.attached?
+        errors.add(:avatar, "required")
+      end
+    end
   end
 end
