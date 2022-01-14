c57690924be364959ad6b9378e2f1ce5a0d64658
Joe Masilotti <joe@masilotti.com>
2022-01-13 20:33:25 -0800

    Depending on when this is run, it might be "about"

--- test/components/availability_component_test.rb
+++ test/components/availability_component_test.rb
@@ -26,7 +26,7 @@ class AvailabilityComponentTest < ViewComponent::TestCase
     render_inline(AvailabilityComponent.new(developer: @developer))
 
     assert_no_selector("svg")
-    assert_selector("span", text: /Available\s*in\s*2 months/m)
+    assert_selector("span", text: /Available\s*in\s*(about\s*)?2 months/m)
   end
 
   test "unavailable icon and color" do
