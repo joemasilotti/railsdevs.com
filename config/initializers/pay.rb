6b169f78a9c0df0036b5791ac172cd83b1a771d8
xmarlox <marlo.p.o.morales@gmail.com>
2022-01-17 12:28:03 +0800

    Revert "Issue#213: More robust database seeds - Remove unused files"
    This reverts commit 5c2e352576f08becd31048752a05443232f84e82.

--- config/initializers/pay.rb
+++ config/initializers/pay.rb
@@ -2,7 +2,7 @@ Pay.setup do |config|
   config.business_name = "railsdevs"
   config.business_address = "2625 SE Market St, Portland, OR 97214"
   config.application_name = "railsdevs"
-  config.support_email = "moralesmarlo@live.com.ph"
+  config.support_email = "joe@masilotti.com"
 
   config.send_emails = true
 
