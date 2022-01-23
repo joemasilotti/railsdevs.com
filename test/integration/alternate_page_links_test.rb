addffb1fa72c9f47fa79ab678214821098fb8e1f
hungmi <hungmihungmi@gmail.com>
2022-01-23 22:08:49 +0800

    Alternate page links in <head> should not show default locale

--- test/integration/alternate_page_links_test.rb
+++ test/integration/alternate_page_links_test.rb
@@ -2,10 +2,10 @@ require "test_helper"
 
 class AlternatePageLinksTest < ActionDispatch::IntegrationTest
   test "all available locales are shown in <head> with correct hreflang" do
-    get root_path(locale: "en")
-    assert_select "head link[href='http://www.example.com/en'][hreflang='x-default']"
+    get root_path(locale: I18n.default_locale)
+    assert_select "head link[href='http://www.example.com/'][hreflang='x-default']"
 
-    other_locales = I18n.available_locales - [:en]
+    other_locales = I18n.available_locales - [I18n.default_locale]
     other_locales.each do |locale|
       assert_select "head link[href='http://www.example.com/#{locale}'][hreflang='#{locale}']"
     end
