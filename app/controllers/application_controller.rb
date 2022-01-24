c8e9031add22452fa3d5d0e5d71eca3a3f06594d
Salomón Charabati <salochara@gmail.com>
2022-01-24 17:36:32 -0600

    Remove switch locale logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   include StoredLocation
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  around_action :switch_locale
 
   def after_sign_in_path_for(user)
     if (stored_location = stored_location_for(:user)).present?
@@ -16,11 +15,6 @@ class ApplicationController < ActionController::Base
   end
 
   private
-
-  def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
-  end
 
   def user_not_authorized
     flash[:alert] = I18n.t("pundit.errors.unauthorized")
