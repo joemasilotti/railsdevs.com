e2b839fb193282b188885b3f941781fa3b34a0f4
hungmi <hungmihungmi@gmail.com>
2022-01-27 02:21:18 +0800

    Remove duplicated resolve_locale helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,4 @@ module ApplicationHelper
   def hotwire_livereload_tags
     super if Rails.env.development?
   end
-
-  def resolve_locale(locale)
-    locale == I18n.default_locale ? nil : locale
-  end
 end
