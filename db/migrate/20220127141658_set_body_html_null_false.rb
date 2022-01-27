35e7bb2f10eb33003399b3012ef9ba7ada6bb3e1
Dennis Paagman <dennis@paagman.dev>
2022-01-27 15:57:05 +0100

    Remove setting empty body_html

--- db/migrate/20220127141658_set_body_html_null_false.rb
+++ db/migrate/20220127141658_set_body_html_null_false.rb
@@ -1,7 +1,5 @@
 class SetBodyHtmlNullFalse < ActiveRecord::Migration[7.0]
   def change
-    execute "UPDATE messages SET body_html = '' WHERE body_html IS NULL"
-
     change_column_null :messages, :body_html, false
   end
 end
