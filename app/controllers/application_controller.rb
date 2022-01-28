ed072eeeb978333685df6dd9d5b68749feb2a752
Hungmi <hungmihungmi@gmail.com>
2022-01-28 12:44:51 +0800

    Update app/controllers/application_controller.rb
    Co-authored-by: Joe Masilotti <joe@masilotti.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   include StoredLocation
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   before_action :set_locale
   helper_method :resolve_locale
 
