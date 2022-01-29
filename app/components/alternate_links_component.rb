b97ff890c791547b87e694cfee5f6312bc1f6b15
Joe Masilotti <joe@masilotti.com>
2022-01-29 15:14:01 -0800

    Extract alternate links to a component

---
+++ app/components/alternate_links_component.rb
@@ -0,0 +1,13 @@
+class AlternateLinksComponent < ApplicationComponent
+  def locales
+    I18n.available_locales
+  end
+
+  def href_for(locale)
+    url_for(locale: helpers.resolve_locale(locale), only_path: false)
+  end
+
+  def hreflang_for(locale)
+    locale == I18n.locale ? "x-default" : locale
+  end
+end
