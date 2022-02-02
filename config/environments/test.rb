3a09f335d955b330a89648428e1a33bcea0116c1
Joe Masilotti <joe@masilotti.com>
2022-02-02 10:55:09 -0800

    Localize /open

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,7 +8,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
