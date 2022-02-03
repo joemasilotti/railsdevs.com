6187bd5e8de663e31655e21bf7e423cf7631d8b8
Salomón Charabati <salochara@gmail.com>
2022-02-03 13:06:05 -0600

    Remove round_to_lower_multiple_of_ten implementation
    - Use Ruby's round() instead

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,4 @@ module ApplicationHelper
   def hotwire_livereload_tags
     super if Rails.env.development?
   end
-
-  def round_to_lower_multiple_of_ten(number)
-    return number - 1 if number <= 10
-
-    modulo = number % 10
-    number - modulo
-  end
 end
