c6dcefcfd53a41f9566df673201cd15dc4431833
Joe Masilotti <joe@masilotti.com>
2022-02-25 13:20:13 -0800

    Render default open graph tags if not set

--- app/helpers/meta_helper.rb
+++ app/helpers/meta_helper.rb
@@ -1,5 +1,6 @@
 module MetaHelper
   def open_graph_tags
-    content_for :open_graph_tags
+    render(OpenGraphTagsComponent.new) unless content_for?(:open_graph_tags)
+    content_for(:open_graph_tags)
   end
 end
