2c3c3b045b8980a1d24bb33cbd0e9e4e214639c2
Mudassar Hassan <mudassarhassan135@gmail.com>
2022-03-07 12:03:31 +0500

    allow role level filters in test env

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -4,7 +4,7 @@ class Feature
     when :new_developer_fields_banner
       true
     when :role_level_filters
-      Rails.env.development?
+      Rails.env.development? || Rails.env.test?
     end
   end
 end
