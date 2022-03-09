9369352dde3e2d2023621c222ff9cb4c414c073d
kaushikhande <kaushikhande302@gmail.com>
2022-03-09 12:46:36 +0530

    remove column time_zone and utc_offset

---
+++ db/migrate/20220309070740_remove_time_zone_and_utc_offset_from_developer.rb
@@ -0,0 +1,6 @@
+class RemoveTimeZoneAndUTCOffsetFromDeveloper < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :developers, :time_zone, :string
+    remove_column :developers, :utc_offset, :integer
+  end
+end
