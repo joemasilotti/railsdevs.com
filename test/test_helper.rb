5e8b174f9d7845f1096f904d3031785711e39b0f
Joe Masilotti <joe@masilotti.com>
2022-03-09 13:04:27 -0800

    Allow Chrome to be downloaded

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,7 +13,10 @@ Dir[Rails.root.join("test/support/**/*.rb")].each { |f| require f }
 options = ENV["REPORTER"].to_s.downcase == "slow" ? {fast_fail: true, slow_count: 5} : {}
 Minitest::Reporters.use!([Minitest::Reporters::PrideReporter.new(options)])
 
-WebMock.disable_net_connect!(allow_localhost: true)
+WebMock.disable_net_connect!({
+  allow_localhost: true,
+  allow: "chromedriver.storage.googleapis.com"
+})
 
 class ActiveSupport::TestCase
   include GeocoderHelper
