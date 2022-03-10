c309922bad779ffb1949addfddd1b841c1b0debb
Joe Masilotti <joe@masilotti.com>
2022-03-10 10:30:41 -0800

    Enable v2 pricing!

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -4,7 +4,7 @@ class Feature
     when :new_developer_fields_banner
       true
     when :pricing_v2
-      !Rails.env.production?
+      true
     when :role_level_filters
       !Rails.env.production?
     end
