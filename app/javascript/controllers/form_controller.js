d6863a66dc39d025d342c39ff5dbaebfc2ec1661
Jason Platts <48892071+jasonplatts@users.noreply.github.com>
2022-03-20 11:46:44 -0400

    Updated controller method name.

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  cmdEnterSubmit(event) {
+  submit(event) {
     if ((event.ctrlKey == true || event.metaKey == true) && event.code == "Enter") {
       event.preventDefault()
       this.element.requestSubmit()
