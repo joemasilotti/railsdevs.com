fe55707dbb385c59d10f2dee85f3635cb69f28a8
Jason Platts <48892071+jasonplatts@users.noreply.github.com>
2022-03-20 11:40:30 -0400

    Changed single quotes to double quotes in form_controller.js

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   cmdEnterSubmit(event) {
-    if ((event.ctrlKey == true || event.metaKey == true) && event.code == 'Enter') {
+    if ((event.ctrlKey == true || event.metaKey == true) && event.code == "Enter") {
       event.preventDefault()
       this.element.requestSubmit()
     }
