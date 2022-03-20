bc6df433a42c84a3f52dc4a1afa6899c72ea8a84
Jason Platts <48892071+jasonplatts@users.noreply.github.com>
2022-03-20 11:39:19 -0400

    Removed comment from form_controller.js.

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,6 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="form"
 export default class extends Controller {
   cmdEnterSubmit(event) {
     if ((event.ctrlKey == true || event.metaKey == true) && event.code == 'Enter') {
