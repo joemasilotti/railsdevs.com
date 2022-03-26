648e48306b33c827dd1cc5b492a3b104064f6041
Moses Gathuku <mosesgathuku95@gmail.com>
2022-03-26 22:15:39 +0300

    toggle copied with a check icon

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,7 +1,22 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["toggleable"];
+
   copy({ target: { value } }) {
     navigator.clipboard.writeText(value);
+  }
+
+  toggle() {
+    this.toggleHidden();
+    setTimeout(() => {
+      this.toggleHidden();
+    }, 2000);
+  }
+
+  toggleHidden() {
+    this.toggleableTargets.forEach((toggleable) => {
+      toggleable.classList.toggle("hidden");
+    });
   }
 }
