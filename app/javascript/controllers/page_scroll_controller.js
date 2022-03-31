57fcf3a73d28144ca77347f16971f14dfef506cf
Joe Masilotti <joe@masilotti.com>
2022-03-30 19:55:24 -0700

    Fix autoscroll with Turbo

--- app/javascript/controllers/page_scroll_controller.js
+++ app/javascript/controllers/page_scroll_controller.js
@@ -1,37 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "scrollPoint", "messageForm" ]
-
   connect() {
-    // this.scrollToBottomOfElement()
-    // this.scrollToTarget()
-    console.log("connected")
-
+    scrollElementIntoView()
   }
 
-  scrollMe() {
-    if (this.messageFormTarget.complete) {
-      this.scrollPointTarget.scrollIntoView(false)
-    } else {
-      this.messageFormTarget.loaded.then(() => {
-        console.log("it's loaded")
-        this.scrollPointTarget.scrollIntoView(false)
-      })
-    }
-  }
-
-  scrollPointTargetConnected(element) {
-    // if element.loaded
-    // console.log("Target Connected")
-    this.scrollPointTarget.scrollIntoView(false)
-    // element.scrollIntoView(false)
-  }
-
-  scrollToTarget() {
-    console.log("Scrolled YEAH!")
-    // this.scrollPointTarget.scrollIntoView(false)
-    this.scrollPointTarget.scrollIntoView(false)
-    console.log("turbo", Turbo)
+  scrollElementIntoView() {
+    this.element.scrollIntoView(false)
   }
 }
