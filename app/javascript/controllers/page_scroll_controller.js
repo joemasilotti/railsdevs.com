6203a8f20bbb0218a81c0c6be1b2d40beff688b0
Jason Platts <48892071+jasonplatts@users.noreply.github.com>
2022-03-30 21:15:09 -0400

    Added page scroll Stimulus controller.

--- app/javascript/controllers/page_scroll_controller.js
+++ app/javascript/controllers/page_scroll_controller.js
@@ -1,11 +1,37 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = [ "scrollPoint", "messageForm" ]
+
   connect() {
-    this.scrollToBottom()
+    // this.scrollToBottomOfElement()
+    // this.scrollToTarget()
+    console.log("connected")
+
   }
 
-  scrollToBottom() {
-    this.element.scrollIntoView(false)
+  scrollMe() {
+    if (this.messageFormTarget.complete) {
+      this.scrollPointTarget.scrollIntoView(false)
+    } else {
+      this.messageFormTarget.loaded.then(() => {
+        console.log("it's loaded")
+        this.scrollPointTarget.scrollIntoView(false)
+      })
+    }
+  }
+
+  scrollPointTargetConnected(element) {
+    // if element.loaded
+    // console.log("Target Connected")
+    this.scrollPointTarget.scrollIntoView(false)
+    // element.scrollIntoView(false)
+  }
+
+  scrollToTarget() {
+    console.log("Scrolled YEAH!")
+    // this.scrollPointTarget.scrollIntoView(false)
+    this.scrollPointTarget.scrollIntoView(false)
+    console.log("turbo", Turbo)
   }
 }
