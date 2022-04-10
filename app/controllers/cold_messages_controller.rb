2485ea0d3e31ce4ad607f39512baca6fbacaad12
kaushikhande <kaushikhande302@gmail.com>
2022-04-10 11:54:48 +0530

    add terms of service

--- app/controllers/cold_messages_controller.rb
+++ app/controllers/cold_messages_controller.rb
@@ -50,6 +50,6 @@ class ColdMessagesController < ApplicationController
   end
 
   def message_params
-    params.require(:message).permit(:body)
+    params.require(:message).permit(:body, :terms_of_service)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,8 @@ class Message < ApplicationRecord
     AutoHtml::SimpleFormat.new
   )
 
+  attr_accessor :terms_of_service
+
   belongs_to :conversation
   belongs_to :sender, polymorphic: true
   has_one :developer, through: :conversation
@@ -13,6 +15,7 @@ class Message < ApplicationRecord
   has_noticed_notifications
 
   validates :body, presence: true
+  validates :terms_of_service, acceptance: 1
 
   after_create_commit :send_recipient_notification
 
