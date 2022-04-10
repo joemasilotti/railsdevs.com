58213ad7675eef48533f0e3c089096ca1a0589f9
Drew Bragg <DRBragg@gmail.com>
2022-04-10 17:44:49 -0400

    Consolidate logic

--- app/controllers/admin/invisiblizes_controller.rb
+++ app/controllers/admin/invisiblizes_controller.rb
@@ -1,14 +1,8 @@
 module Admin
   class InvisiblizesController < ApplicationController
     def create
-      developer.invisible!
+      Developer.find(params[:developer_id]).invisible!
       redirect_to developers_path, notice: t(".created")
-    end
-
-    private
-
-    def developer
-      @developer ||= Developer.find(params[:developer_id])
     end
   end
 end
