87ac8975bd8d13f814bc829e2698ed38504330e5
Joe Masilotti <joe@masilotti.com>
2022-04-13 20:12:48 -0700

    Replace message form with upgrade alert

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -8,8 +8,7 @@ class ConversationsController < ApplicationController
 
   def show
     @conversation = conversation
-    @message = Message.new
-    @message.conversation = conversation
+    @message = Message.new(conversation: @conversation)
     authorize @conversation, policy_class: MessagingPolicy
     @messageable = SubscriptionPolicy.new(current_user, @message).messageable?
   end
