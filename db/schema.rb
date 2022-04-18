56c5049aaa84b70f1ca581ad16e20f765e80e1f8
Joe Masilotti <joe@masilotti.com>
2022-04-18 11:03:04 -0700

    Check in schema changes from previous migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_16_144523) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -224,7 +225,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_16_144523) do
     t.string "processor_id"
     t.boolean "default"
     t.jsonb "data"
-    t.datetime "deleted_at"
+    t.datetime "deleted_at", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["owner_type", "owner_id", "deleted_at", "default"], name: "pay_customer_owner_index"
@@ -261,8 +262,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_16_144523) do
     t.string "processor_plan", null: false
     t.integer "quantity", default: 1, null: false
     t.string "status", null: false
-    t.datetime "trial_ends_at"
-    t.datetime "ends_at"
+    t.datetime "trial_ends_at", precision: nil
+    t.datetime "ends_at", precision: nil
     t.decimal "application_fee_percent", precision: 8, scale: 2
     t.jsonb "metadata"
     t.jsonb "data"
