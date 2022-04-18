80b9e770e78b037cca42218ab3c5baaa5b212b6a
Joe Masilotti <joe@masilotti.com>
2022-04-18 12:17:44 -0700

    Hiring fee CTA (#380)
    * Extract HasBusinessSubscription
    
    * Extract scope
    
    * CTA banner to pay hiring fee
    
    * Add notes to CHANGELOG

---
+++ test/components/hiring_fee_component_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class HiringFeeComponentTest < ViewComponent::TestCase
+  setup do
+    @conversation = conversations(:one)
+    @user = @conversation.business.user
+  end
+
+  test "renders if full-time subscription and is eligible for the fee" do
+    @conversation.update!(created_at: 2.weeks.ago - 1.day)
+
+    render_inline HiringFeeComponent.new(@user, @conversation)
+    assert_text "Have you hired"
+  end
+
+  test "doesn't render if not on a full-time subscription" do
+    @conversation.update!(created_at: 2.weeks.ago - 1.day)
+    pay_subscriptions(:full_time).update!(name: "part_time")
+
+    render_inline HiringFeeComponent.new(@user, @conversation)
+    assert_no_text "Have you hired"
+  end
+
+  test "doesn't render if the conversation is not eligible for the fee" do
+    render_inline HiringFeeComponent.new(@user, @conversation)
+    assert_no_text "Have you hired"
+  end
+end
