f60da0cf107b4ca7c672faf2274770fd30130c95
Joe Masilotti <joe@masilotti.com>
2022-05-06 10:39:52 -0700

    Make subscription seeds idempotent

--- db/seeds/businesses.rb
+++ db/seeds/businesses.rb
@@ -2,13 +2,17 @@
 business = SeedsHelper.create_business!("business", {
   developer_notifications: :daily
 })
-business.user.set_payment_processor(:fake_processor, allow_fake: true)
-business.user.payment_processor.subscribe(plan: BusinessSubscription::FullTime.new.plan)
+if business.user.subscriptions.none?
+  business.user.set_payment_processor(:fake_processor, allow_fake: true)
+  business.user.payment_processor.subscribe(plan: BusinessSubscription::FullTime.new.plan)
+end
 
 # Business on part-time plan (part-time)
 business = SeedsHelper.create_business!("part-time")
-business.user.set_payment_processor(:fake_processor, allow_fake: true)
-business.user.payment_processor.subscribe(plan: BusinessSubscription::PartTime.new.plan)
+if business.user.subscriptions.none?
+  business.user.set_payment_processor(:fake_processor, allow_fake: true)
+  business.user.payment_processor.subscribe(plan: BusinessSubscription::PartTime.new.plan)
+end
 
 # Lead business (lead)
 SeedsHelper.create_business!("lead")
