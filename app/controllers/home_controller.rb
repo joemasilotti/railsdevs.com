51b7d00857a0fdad562e57d8bce83ca5311c8fad
Joe Masilotti <joe@masilotti.com>
2022-05-08 22:29:08 -0700

    Rework testimonials to "success stories"

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,12 @@ class HomeController < ApplicationController
       .includes(:role_type).with_attached_avatar
       .available.newest_first
       .limit(10)
+    @hired_developers = YAML.load_file(hired_developers_yaml)
+  end
+
+  private
+
+  def hired_developers_yaml
+    File.join(Rails.root, "app", "data", "hired_developers.yml")
   end
 end
