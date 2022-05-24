42c3a833b5a38c4d1c7a63b9216ca2c0c2a02598
Joe Masilotti <joe@masilotti.com>
2022-05-24 12:44:54 -0700

    Remove last bit of HasBusinessSubscription

--- test/components/legacy_business_subscription_component_test.rb
+++ test/components/legacy_business_subscription_component_test.rb
@@ -1,17 +1,24 @@
 require "test_helper"
 
 class LegacyBusinessSubscriptionComponentTest < ViewComponent::TestCase
+  include SubscriptionsHelper
+
+  setup do
+    @user = users(:subscribed_business)
+  end
+
   test "renders if the user is subscribed to a legacy business subscription" do
-    user = User.new(subscribed: true)
-    render_inline LegacyBusinessSubscriptionComponent.new(user) do
+    update_subscription(:legacy)
+
+    render_inline LegacyBusinessSubscriptionComponent.new(@user) do
       "Rendered!"
     end
+
     assert_text "Rendered!"
   end
 
   test "does not render otherwise" do
-    user = User.new(subscribed: false)
-    render_inline LegacyBusinessSubscriptionComponent.new(user) do
+    render_inline LegacyBusinessSubscriptionComponent.new(@user) do
       "Rendered!"
     end
     assert_selector "*", count: 0
@@ -20,15 +27,5 @@ class LegacyBusinessSubscriptionComponentTest < ViewComponent::TestCase
       "Rendered!"
     end
     assert_selector "*", count: 0
-  end
-
-  class User
-    def initialize(subscribed:)
-      @subscribed = subscribed
-    end
-
-    def active_legacy_business_subscription?
-      @subscribed
-    end
   end
 end
