c96b4641de3cb24336a01a80d3e20cad05559ae9
Joe Masilotti <joe@masilotti.com>
2022-05-28 10:42:02 -0700

    Fix seeds to work with new subscription modeling

--- db/seeds/businesses.rb
+++ db/seeds/businesses.rb
@@ -5,7 +5,7 @@ business = SeedsHelper.create_business!("business", {
 if business.user.subscriptions.none?
   business.user.set_payment_processor(:fake_processor, allow_fake: true)
   business.user.payment_processor
-    .subscribe(plan: Businesses::Subscription.with_identifier(:full_time))
+    .subscribe(plan: Businesses::Subscription.with_identifier(:full_time).price_id)
 end
 
 # Business on part-time plan (part-time)
@@ -13,7 +13,7 @@ business = SeedsHelper.create_business!("part-time")
 if business.user.subscriptions.none?
   business.user.set_payment_processor(:fake_processor, allow_fake: true)
   business.user.payment_processor
-    .subscribe(plan: Businesses::Subscription.with_identifier(:part_time))
+    .subscribe(plan: Businesses::Subscription.with_identifier(:part_time).price_id)
 end
 
 # Lead business (lead)
