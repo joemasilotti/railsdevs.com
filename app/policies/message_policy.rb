26fe5052377b795fe6e9b6da885e2f7dbad19cb7
Kasper Timm Hansen <kaspth@gmail.com>
2022-05-29 21:25:15 +0200

    A conversation is either to a developer or business, prefer clarity now versus flexibility for later

--- app/policies/message_policy.rb
+++ app/policies/message_policy.rb
@@ -1,10 +1,7 @@
 class MessagePolicy < ApplicationPolicy
   def create?
-    return true if developer_recipient?
-
-    return false unless business_recipient?
-
-    permission.can_message_developer?(role_type: conversation.developer.role_type)
+    developer_recipient? ||
+      permission.can_message_developer?(role_type: conversation.developer.role_type)
   end
 
   private
@@ -13,10 +10,6 @@ class MessagePolicy < ApplicationPolicy
 
   def developer_recipient?
     conversation.developer?(user)
-  end
-
-  def business_recipient?
-    conversation.business?(user)
   end
 
   def permission
