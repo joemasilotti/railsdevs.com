85bfe8bf954dcea0d553e3056ed77cd0b79ae76c
Joe Masilotti <joe@masilotti.com>
2022-05-29 19:47:50 -0700

    WIP

--- app/components/developer_primary_action_component.rb
+++ app/components/developer_primary_action_component.rb
@@ -1,10 +1,18 @@
 class DeveloperPrimaryActionComponent < ApplicationComponent
-  def initialize(user:, developer:)
+  attr_reader :user, :developer, :business
+
+  def initialize(user:, developer:, business:)
     @user = user
     @developer = developer
+    @business = business
   end
 
   def owner?
-    @user&.developer == @developer
+    user&.developer == developer
+  end
+
+  def conversation
+    return nil
+    Conversation.find_by(developer:, business:)
   end
 end
