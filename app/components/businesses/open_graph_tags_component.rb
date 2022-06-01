0b84a62793adf6fb713def677984054004fec6b7
Monica Mateiu <monica.mateiu@gmail.com>
2022-06-01 08:08:49 +0100

    move components to Business namespace

--- app/components/businesses/open_graph_tags_component.rb
+++ app/components/businesses/open_graph_tags_component.rb
@@ -1,21 +1,23 @@
-class OpenGraphTagsComponent < OpenGraphTagsComponent
-  def initialize(business:)
-    @business = business
-  end
+module Businesses
+  class OpenGraphTagsComponent < OpenGraphTagsComponent
+    def initialize(business:)
+      @business = business
+    end
 
-  def title
-    @business.company
-  end
+    def title
+      @business.company
+    end
 
-  def description
-    @business.bio
-  end
+    def description
+      @business.bio
+    end
 
-  def url
-    business_url(@business)
-  end
+    def url
+      business_url(@business)
+    end
 
-  def image
-    rails_blob_url(@business.avatar) if @business.avatar.attached?
+    def image
+      rails_blob_url(@business.avatar) if @business.avatar.attached?
+    end
   end
 end
---
+++ test/components/businesses/legacy_business_subscription_component_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+module Businesses
+  class LegacyBusinessSubscriptionComponentTest < ViewComponent::TestCase
+    include SubscriptionsHelper
+
+    test "renders if the user is subscribed to a legacy business subscription" do
+      user = users(:subscribed_business)
+      update_subscription(:legacy)
+
+      render_inline LegacyBusinessSubscriptionComponent.new(user) do
+        "Rendered!"
+      end
+      assert_text "Rendered!"
+    end
+
+    test "does not render otherwise" do
+      user = users(:subscribed_business)
+      render_inline LegacyBusinessSubscriptionComponent.new(user) do
+        "Rendered!"
+      end
+      assert_selector "*", count: 0
+
+      render_inline LegacyBusinessSubscriptionComponent.new(nil) do
+        "Rendered!"
+      end
+      assert_selector "*", count: 0
+    end
+  end
+end
