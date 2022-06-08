b14fc950e606e23d741579977b61b01cf07579a4
Joe Masilotti <joe@masilotti.com>
2022-06-08 11:18:08 -0700

    Remove data migration

--- db/migrate/20220530153119_create_conversation_blocks.rb
+++ db/migrate/20220530153119_create_conversation_blocks.rb
@@ -7,13 +7,5 @@ class CreateConversationBlocks < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
-
-    Conversation.where.not(business_blocked_at: nil).or(Conversation.where.not(developer_blocked_at: nil)).find_each do |conversation|
-      if conversation.business_blocked_at?
-        Conversation::Block.create! conversation:, blocker: conversation.business.user, blockee: conversation.developer.user, created_at: conversation.business_blocked_at
-      elsif conversation.developer_blocked_at?
-        Conversation::Block.create! conversation:, blocker: conversation.developer.user, blockee: conversation.business.user, created_at: conversation.developer_blocked_at
-      end
-    end
   end
 end
