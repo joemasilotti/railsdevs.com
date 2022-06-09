b540dfb35e5cc0a0778da0072df29333a883352f
Joe Masilotti <joe@masilotti.com>
2022-06-09 13:57:03 -0700

    Add happy path test

--- test/mailboxes/messages_mailbox_test.rb
+++ test/mailboxes/messages_mailbox_test.rb
@@ -1,11 +1,20 @@
 require "test_helper"
 
 class MessagesMailboxTest < ActionMailbox::TestCase
-  # test "receive mail" do
-  #   receive_inbound_email_from_mail \
-  #     to: '"someone" <someone@example.com>',
-  #     from: '"else" <else@example.com>',
-  #     subject: "Hello world!",
-  #     body: "Hello?"
-  # end
+  test "a recipient can continue the conversation" do
+    conversation = conversations(:one)
+    signed_conversation_id = conversation.signed_id(purpose: :message)
+
+    assert_changes "Message.count", 1 do
+      receive_inbound_email_from_mail \
+        to: "message-#{signed_conversation_id}@example.com",
+        from: conversation.developer.user.email,
+        body: "Email body."
+    end
+
+    message = Message.last
+    assert_equal conversation, message.conversation
+    assert_equal conversation.developer, message.sender
+    assert_equal "Email body.", message.body
+  end
 end
