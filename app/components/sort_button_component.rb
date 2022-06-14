ff310ed4839e7d86d7bc87c0783ea0d513b10735
Joe Masilotti <joe@masilotti.com>
2022-06-13 20:20:55 -0700

    Pass along form ID/scope to sort button
    so it can be merged in correctly in DevelopersController.

--- app/components/sort_button_component.rb
+++ app/components/sort_button_component.rb
@@ -1,12 +1,21 @@
 class SortButtonComponent < ApplicationComponent
-  attr_reader :title, :name, :value, :form_id
+  attr_reader :title, :value, :form_id
 
-  def initialize(title:, name:, value:, active:, form_id:)
+  def initialize(title:, name:, value:, active:, form_id:, scope: nil)
     @title = title
     @name = name
     @value = value
     @active = active
     @form_id = form_id
+    @scope = scope
+  end
+
+  def name
+    if @scope.present?
+      "#{@scope}[#{@name}]"
+    else
+      @name
+    end
   end
 
   def dynamic_classes
