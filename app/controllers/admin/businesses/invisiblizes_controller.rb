e1fa9e2557eafa7835e6fbb59f4cc2752d5caead
Drew Bragg <DRBragg@gmail.com>
2022-06-22 19:43:06 -0400

    fix: Drop unneeded namespace

--- app/controllers/admin/businesses/invisiblizes_controller.rb
+++ app/controllers/admin/businesses/invisiblizes_controller.rb
@@ -1,6 +1,6 @@
 module Admin
   module Businesses
-    class InvisiblizesController < Admin::ApplicationController
+    class InvisiblizesController < ApplicationController
       def create
         Business.find(params[:business_id]).invisiblize_and_notify!
         redirect_to root_path, notice: t(".created")
