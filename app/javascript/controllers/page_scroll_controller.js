fe311647bdeced62a94ef5c94585f84754d27ee0
Paul Kim <pkayokay@gmail.com>
2022-06-24 08:54:57 -0400

    Update controller to scroll on connect

--- app/javascript/controllers/page_scroll_controller.js
+++ app/javascript/controllers/page_scroll_controller.js
@@ -2,14 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    document.addEventListener("turbo:load", this.scrollIntoView)
-  }
-
-  disconnect() {
-    document.removeEventListener("turbo:load", this.scrollIntoView)
-  }
-
-  scrollIntoView() {
     this.element.scrollIntoView(false)
   }
 }
