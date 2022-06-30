135f6bfe0fe124e1e7b0be007a0d9d9da68ca759
Paul Kim <pkayokay@gmail.com>
2022-06-29 21:59:55 -0400

    Set a timeout when controller connects to run scroll on separate thread

--- app/javascript/controllers/page_scroll_controller.js
+++ app/javascript/controllers/page_scroll_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.scrollIntoView(false)
+    setTimeout(() => {
+      this.element.scrollIntoView(false)
+    }, 0);
   }
 }
