c2054e4c910faa21046c5476e56727a53762d822
Adrien Poly <adrienpoly@gmail.com>
2022-06-30 06:27:05 +0200

    🚨 run standard rb -fix

--- test/components/developers/search_status_component_test.rb
+++ test/components/developers/search_status_component_test.rb
@@ -9,19 +9,19 @@ module Developers
     test "actively looking" do
       @developer.search_status = :actively_looking
       render_inline SearchStatusComponent.new(@developer)
-      assert_text /actively looking/i
+      assert_text(/actively looking/i)
     end
 
     test "open" do
       @developer.search_status = :open
       render_inline SearchStatusComponent.new(@developer)
-      assert_text /open/i
+      assert_text(/open/i)
     end
 
     test "not interested" do
       @developer.search_status = :not_interested
       render_inline SearchStatusComponent.new(@developer)
-      assert_text /not interested/i
+      assert_text(/not interested/i)
     end
 
     test "none" do
