d1a28e8c8a3c744c33da040dca4ec3263f22e68d
Adrien Poly <adrienpoly@gmail.com>
2022-07-10 23:23:55 +0200

    🐛 add missing bind(this)

--- app/javascript/controllers/page_scroll_controller.js
+++ app/javascript/controllers/page_scroll_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    document.addEventListener("turbo:load", this.scrollIntoView)
+    document.addEventListener("turbo:load", this.scrollIntoView.bind(this))
   }
 
   disconnect() {
