40447e9e4bd6fde91b0168bdb2a9588965447135
Paul Kim <phmk@hey.com>
2022-07-11 18:11:47 -0400

    Persist scroll position when navigating backwards or forwards

--- app/javascript/controllers/page_scroll_controller.js
+++ app/javascript/controllers/page_scroll_controller.js
@@ -4,23 +4,29 @@ export default class extends Controller {
   static values = {
     onConnect: { type: Boolean, default: true },
     focus: { type: Boolean, default: true },
+    shouldScroll: { type: Boolean, default: true },
   }
 
   connect() {
     requestAnimationFrame(() => {
-      if (this.onConnectValue) this.scrollIntoView()
-      if (this.focusValue) this.focus()
+      if (this.shouldScrollValue) {
+        if (this.onConnectValue) this.scrollIntoView()
+        if (this.focusValue) this.focus()
+      }
+
+      // Persit scroll position when navigating backwards or forwards
+      window.onpopstate = (e) => {
+        this.shouldScrollValue = false
+      }
     })
   }
-
-  // actions
 
   scrollIntoView() {
     this.element.scrollIntoView(false)
   }
 
   focus() {
-    // focus on the first input , select or textarea in the element
+    // Focus on the first input , select or textarea in the element
     this.element.querySelector("input, select, textarea").focus()
   }
 }
