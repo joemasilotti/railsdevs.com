3d5fb0f3f6e497f587aea7ab5bdd8da4606721dc
Paul Kim <phmk@hey.com>
2022-07-11 18:00:23 -0400

    Maintain focus on text area after submission

--- app/javascript/controllers/page_scroll_controller.js
+++ app/javascript/controllers/page_scroll_controller.js
@@ -1,9 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static values = {
+    onConnect: { type: Boolean, default: true },
+    focus: { type: Boolean, default: true },
+  }
+
   connect() {
     requestAnimationFrame(() => {
-      this.element.scrollIntoView(false)
+      if (this.onConnectValue) this.scrollIntoView()
+      if (this.focusValue) this.focus()
     })
+  }
+
+  // actions
+
+  scrollIntoView() {
+    this.element.scrollIntoView(false)
+  }
+
+  focus() {
+    // focus on the first input , select or textarea in the element
+    this.element.querySelector("input, select, textarea").focus()
   }
 }
