0b4db71f8d1a67d259b4bb9ca2dad7cd072a6c4c
Joe Masilotti <joe@masilotti.com>
2022-07-10 19:33:59 -0700

    Only configure Faker when it's defined (dev/test)

--- config/initializers/faker.rb
+++ config/initializers/faker.rb
@@ -1,2 +1,4 @@
-# Ensure that all "random" seeded developers are always the same.
-Faker::Config.random = Random.new("railsdevs".bytes.sum)
+if Object.const_defined?("Faker")
+  # Ensure that all "random" seeded developers are always the same.
+  Faker::Config.random = Random.new("railsdevs".bytes.sum)
+end
