82b0d992eed67c4f8dbb1def476ac0aa24de5ecf
Adrien Poly <adrienpoly@gmail.com>
2022-07-11 04:30:30 +0200

    Add 1x and 2x avatar image variant (#555)
    * ✨ add 1x and 2x avatar image variant
    
    * Backfill new variants, too
    
    Co-authored-by: Joe Masilotti <joe@masilotti.com>

--- app/models/concerns/avatarable.rb
+++ app/models/concerns/avatarable.rb
@@ -3,8 +3,10 @@ module Avatarable
 
   included do
     has_one_attached :avatar do |attachable|
-      attachable.variant :thumb, resize_to_limit: [64, 64]
-      attachable.variant :medium, resize_to_limit: [256, 256]
+      attachable.variant :thumb, resize_to_limit: [32, 32]
+      attachable.variant :thumb_2x, resize_to_limit: [64, 64]
+      attachable.variant :medium, resize_to_limit: [128, 128]
+      attachable.variant :medium_2x, resize_to_limit: [256, 256]
     end
 
     validates :avatar, content_type: ["image/png", "image/jpeg", "image/jpg"],
