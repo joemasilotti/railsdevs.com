80d574f43a2b2e4c23f26f8cec87f67d96508dac
Joe Masilotti <joe@masilotti.com>
2022-07-10 19:38:11 -0700

    Standardize

--- config/initializers/faker.rb
+++ config/initializers/faker.rb
@@ -1,4 +1,4 @@
-if Object.const_defined?("Faker")
+if Object.const_defined?(:Faker)
   # Ensure that all "random" seeded developers are always the same.
   Faker::Config.random = Random.new("railsdevs".bytes.sum)
 end
