afd8f07ee733f7852b2f57b94207a71852a62c8f
Joe Masilotti <joe@masilotti.com>
2022-07-12 15:20:16 -0700

    Remove temporary data migration column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_12_221543) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_12_221754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -180,10 +180,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_221543) do
 
   create_table "open_startup_metrics", force: :cascade do |t|
     t.date "occurred_on", null: false
-    t.jsonb "data", default: {}, null: false
+    t.jsonb "legacy_data", default: {}, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.jsonb "data_json"
+    t.jsonb "data", null: false
   end
 
   create_table "open_startup_monthly_balances", force: :cascade do |t|
--- lib/tasks/backfills.rake
+++ lib/tasks/backfills.rake
@@ -1,9 +1,3 @@
 desc "These tasks are meant to be run once then removed"
 namespace :backfills do
-  desc "Migrate OpenStartup::Metric#data to JSON"
-  task store: :environment do
-    OpenStartup::Metric.find_each do |metric|
-      metric.update!(data_json: metric.data)
-    end
-  end
 end
