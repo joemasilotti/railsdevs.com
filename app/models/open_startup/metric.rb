bf5e0bca35e7a371381af75945cb60807c48a875
Joe Masilotti <joe@masilotti.com>
2022-07-14 12:00:17 -0700

    Revert "remove handcoded json (#575)" (#576)
    This reverts commit c19a0b77dc70f4197cb0ec51ca819815d7c0246e.

--- app/models/open_startup/metric.rb
+++ app/models/open_startup/metric.rb
@@ -2,7 +2,7 @@ module OpenStartup
   class Metric < ApplicationRecord
     self.table_name = "open_startup_metrics"
 
-    store_accessor :data, :mrr, :visitors
+    store :data, accessors: %i[mrr visitors], coder: JSON
 
     validates :occurred_on, presence: true
 
