fb5edd458fafac9bfd3881e62f2604406b24b51c
EthanChenYen-Peng <ypcethan@gmail.com>
2022-07-17 11:07:10 +1000

    normalize stack overflow profile link

--- test/models/concerns/has_social_profiles_test.rb
+++ test/models/concerns/has_social_profiles_test.rb
@@ -4,12 +4,13 @@ class HasSocialProfilesTest < ActiveSupport::TestCase
   class Model
     include ActiveModel
 
-    attr_accessor :github, :twitter, :linkedin
+    attr_accessor :github, :twitter, :linkedin, :stack_overflow
 
     def save
       normalize_github
       normalize_twitter
       normalize_linkedin
+      normalize_stack_overflow
     end
 
     def self.before_save(method, options)
@@ -24,58 +25,91 @@ class HasSocialProfilesTest < ActiveSupport::TestCase
   end
 
   test "normalizes GitHub handle" do
+    expected = "joemasilotti"
     @model.github = "joemasilotti"
-    save_and_assert_normalized_handle(:github)
+    save_and_assert_normalized_handle(:github, expected)
 
     @model.github = "http://github.com/joemasilotti"
-    save_and_assert_normalized_handle(:github)
+    save_and_assert_normalized_handle(:github, expected)
 
     @model.github = "https://github.com/joemasilotti"
-    save_and_assert_normalized_handle(:github)
+    save_and_assert_normalized_handle(:github, expected)
 
     @model.github = "http://www.github.com/joemasilotti"
-    save_and_assert_normalized_handle(:github)
+    save_and_assert_normalized_handle(:github, expected)
 
     @model.github = "https://www.github.com/joemasilotti"
-    save_and_assert_normalized_handle(:github)
+    save_and_assert_normalized_handle(:github, expected)
   end
 
   test "normalizes Twitter handle" do
+    expected = "joemasilotti"
     @model.twitter = "joemasilotti"
-    save_and_assert_normalized_handle(:twitter)
+    save_and_assert_normalized_handle(:twitter, expected)
 
     @model.twitter = "http://twitter.com/joemasilotti"
-    save_and_assert_normalized_handle(:twitter)
+    save_and_assert_normalized_handle(:twitter, expected)
 
     @model.twitter = "https://twitter.com/joemasilotti"
-    save_and_assert_normalized_handle(:twitter)
+    save_and_assert_normalized_handle(:twitter, expected)
 
     @model.twitter = "http://www.twitter.com/joemasilotti"
-    save_and_assert_normalized_handle(:twitter)
+    save_and_assert_normalized_handle(:twitter, expected)
 
     @model.twitter = "https://www.twitter.com/joemasilotti"
-    save_and_assert_normalized_handle(:twitter)
+    save_and_assert_normalized_handle(:twitter, expected)
   end
 
   test "normalizes LinkedIn handle" do
+    expected = "joemasilotti"
     @model.linkedin = "joemasilotti"
-    save_and_assert_normalized_handle(:linkedin)
+    save_and_assert_normalized_handle(:linkedin, expected)
 
     @model.linkedin = "http://linkedin.com/in/joemasilotti"
-    save_and_assert_normalized_handle(:linkedin)
+    save_and_assert_normalized_handle(:linkedin, expected)
 
     @model.linkedin = "https://linkedin.com/in/joemasilotti"
-    save_and_assert_normalized_handle(:linkedin)
+    save_and_assert_normalized_handle(:linkedin, expected)
 
     @model.linkedin = "http://www.linkedin.com/in/joemasilotti"
-    save_and_assert_normalized_handle(:linkedin)
+    save_and_assert_normalized_handle(:linkedin, expected)
 
     @model.linkedin = "https://www.linkedin.com/in/joemasilotti"
-    save_and_assert_normalized_handle(:linkedin)
+    save_and_assert_normalized_handle(:linkedin, expected)
   end
 
-  def save_and_assert_normalized_handle(platform)
+  test "normalizes Stack Overflow handle" do
+    expected = "123456"
+    @model.stack_overflow = "123456"
+    save_and_assert_normalized_handle(:stack_overflow, expected)
+
+    @model.stack_overflow = "http://stackoverflow.com/users/123456/joemasilotti"
+    save_and_assert_normalized_handle(:stack_overflow, expected)
+
+    @model.stack_overflow = "https://stackoverflow.com/users/123456/joemasilotti"
+    save_and_assert_normalized_handle(:stack_overflow, expected)
+
+    @model.stack_overflow = "http://stackoverflow.com/users/123456"
+    save_and_assert_normalized_handle(:stack_overflow, expected)
+
+    @model.stack_overflow = "https://stackoverflow.com/users/123456"
+    save_and_assert_normalized_handle(:stack_overflow, expected)
+
+    @model.stack_overflow = "http://www.stackoverflow.com/users/123456/joemasilotti"
+    save_and_assert_normalized_handle(:stack_overflow, expected)
+
+    @model.stack_overflow = "https://www.stackoverflow.com/users/123456/joemasilotti"
+    save_and_assert_normalized_handle(:stack_overflow, expected)
+
+    @model.stack_overflow = "http://www.stackoverflow.com/users/123456"
+    save_and_assert_normalized_handle(:stack_overflow, expected)
+
+    @model.stack_overflow = "https://www.stackoverflow.com/users/123456"
+    save_and_assert_normalized_handle(:stack_overflow, expected)
+  end
+
+  def save_and_assert_normalized_handle(platform, expected)
     @model.save
-    assert_equal @model.send(platform), "joemasilotti"
+    assert_equal @model.send(platform), expected
   end
 end
