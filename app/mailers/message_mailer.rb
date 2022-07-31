25af33f05b9e315a5e15a9bf626907e059e28214
Joe Masilotti <joe@masilotti.com>
2022-07-30 21:45:53 -0700

    Reorganize rendered messages in emails

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,16 +1,14 @@
 class MessageMailer < ApplicationMailer
   default from: Rails.configuration.emails.notifications_mailbox!
-  helper :messages
 
   def new_message
     @notification = params[:record].to_notification
     @recipient = params[:recipient]
 
-    message = @notification.message
-    @sender = message.sender.name
-    @body = message.body
+    @message = @notification.message
+    @sender = @message.sender.name
 
-    conversation_token = message.conversation.inbound_email_token
+    conversation_token = @message.conversation.inbound_email_token
 
     mail(
       to: @recipient.email,
