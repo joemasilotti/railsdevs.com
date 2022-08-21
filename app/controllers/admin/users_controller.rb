bf6810c688155ce680e94280e1d91734b3c9f197
Joe Masilotti <joe@masilotti.com>
2022-08-21 06:44:53 -0700

    Remove N+1 queries on /admin/users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,7 +10,9 @@ module Admin
     private
 
     def users(query)
-      User.filter_by_email(query).order(:email)
+      User.filter_by_email(query)
+        .includes(developer: {avatar_attachment: :blob}, business: {avatar_attachment: :blob})
+        .order(:email)
     end
   end
 end
