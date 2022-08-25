42ee01628870c1b2a83fa0f7946386560dbd64e6
Joe Masilotti <joe@masilotti.com>
2022-08-25 14:40:06 -0700

    Remove dead comments

--- app/notifications/application_notification.rb
+++ app/notifications/application_notification.rb
@@ -1,11 +1,3 @@
 class ApplicationNotification < Noticed::Base
   include UrlHelpersWithDefaultUrlOptions
-
-  # def self.deliver_by(method, **args)
-  # if method == :email
-  # binding.pry
-  # else
-  # super
-  # end
-  # end
 end
