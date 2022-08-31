3c4a3ce4e8522bdb66f8fce044a0b0573b50186f
Joe Masilotti <joe@masilotti.com>
2022-08-31 10:14:49 -0700

    Extract helper method

--- app/controllers/admin/hiring_agreements/terms/activations_controller.rb
+++ app/controllers/admin/hiring_agreements/terms/activations_controller.rb
@@ -3,15 +3,19 @@ module Admin
     module Terms
       class ActivationsController < ApplicationController
         def create
-          term = ::HiringAgreements::Term.find(params[:term_id])
           term.activate!
           redirect_to edit_admin_hiring_agreements_term_path(term)
         end
 
         def destroy
-          term = ::HiringAgreements::Term.find(params[:term_id])
           term.deactivate!
           redirect_to edit_admin_hiring_agreements_term_path(term)
+        end
+
+        private
+
+        def term
+          @term ||= ::HiringAgreements::Term.find(params[:term_id])
         end
       end
     end
