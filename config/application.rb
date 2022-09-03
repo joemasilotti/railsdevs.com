f96944b02892979dc64c7fb7eeba208c64b544e3
Facundo Espinosa <facundo.espinosa@fing.edu.uy>
2022-09-03 15:19:53 -0300

    Configure ViewComponent preview layout

--- config/application.rb
+++ config/application.rb
@@ -51,5 +51,10 @@ module Railsdevs
 
     # Use default language as fallback if translation is missing
     config.i18n.fallbacks = true
+
+    # Defer loading of images until it reaches a calculated distance from the viewport
+    config.action_view.image_loading = "lazy"
+
+    config.view_component.default_preview_layout = "components_previews"
   end
 end
