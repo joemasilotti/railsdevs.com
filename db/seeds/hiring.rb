e8c1094810cd44b88662ef601fcad717250eabbd
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-09-03 22:38:14 -0400

    Updated Hiring Form seed to trigger notification

--- db/seeds/hiring.rb
+++ db/seeds/hiring.rb
@@ -7,6 +7,7 @@ Hired::Form.find_or_create_by!(developer:) do |form|
   form.start_date = Date.today
   form.employment_type = :full_time
   form.feedback = "Keep up the great work with RailsDevs!"
+  form.save_and_notify
 end
 
 HiringAgreements::Term.first_or_create! do |term|
