bdb33d601f237995b19873eb4ae60afc074f64db
Joe Masilotti <joe@masilotti.com>
2022-09-07 20:42:45 -0700

    More modular v1 vs. v2 pricing

--- app/controllers/pricing_controller.rb
+++ app/controllers/pricing_controller.rb
@@ -1,4 +1,15 @@
 class PricingController < ApplicationController
   def show
+    render template
+  end
+
+  private
+
+  def template
+    if Feature.enabled?(:pricing_v2, user: current_user)
+      "pricing/v2/show"
+    else
+      "pricing/v1/show"
+    end
   end
 end
