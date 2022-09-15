48d9baa44cc51ced1e208237c8a841d32c67fb19
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-09-16 06:59:37 +0800

    update doc link and changelog type

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@
 
 ## September
 
-* September 15 - Allow rich text for Develoepr#bio #638 @rayhanw
+* September 15 - Allow rich text for developer#bio #638 @rayhanw
 * September 14 - Only ever send one Celebration Promotion email #617 @sarvaiyanidhi
 * September 13 - Impersonate users directly from `/admin/users` #627 @KarlHeitmann
 * September 13 - Move developer bio to own card in form and add more instructions #635 @rayhanw
--- app/models/concerns/developers/rich_text.rb
+++ app/models/concerns/developers/rich_text.rb
@@ -6,20 +6,21 @@ module Developers
 
     private
 
+    # https://github.com/vmg/redcarpet#darling-i-packed-you-a-couple-renderers-for-lunch
     MARKDOWN_OPTIONS = {
-      disable_indented_code_blocks: true, # Disabled convertion of four spaces to code block
-      fenced_code_blocks: true, # Allows multi-line codeblock (with 3 ` or 3 ~)
-      highlight: true, # Allows highlighting
-      strikethrough: true, # Allows strikethrough
-      superscript: true, # Disables convertion of text with four spaces at the front of each line to code blocks.
-      underline: true # Allows underline
+      disable_indented_code_blocks: true,
+      fenced_code_blocks: true,
+      highlight: true,
+      strikethrough: true,
+      superscript: true,
+      underline: true
     }.freeze
     RENDER_OPTIONS = {
-      filter_html: true, # Disallows manually written HTML tags
-      hard_wrap: true, # Adds newline everytime "Return" is hit
-      no_images: true, # Disables images
-      no_links: true, # Disables links
-      no_styles: true # Disables custom styling
+      filter_html: true,
+      hard_wrap: true,
+      no_images: true,
+      no_links: true,
+      no_styles: true
     }.freeze
 
     def renderer
