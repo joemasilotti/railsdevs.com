23a96dcad875eca1a07d41edbf603494c2def1db
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-09-16 14:25:28 -0400

    Cleaned badge component to only pass title and color attributes

--- app/components/developers/badge_component.rb
+++ app/components/developers/badge_component.rb
@@ -1,14 +1,14 @@
 module Developers
   class BadgeComponent < ApplicationComponent
-    private attr_reader :title, :classes
+    private attr_reader :title, :color
 
-    def initialize(title, classes)
+    def initialize(title, color:)
       @title = title
-      @classes = classes
+      @color = color
     end
 
     def call
-      tag.span title, class: classes.to_s
+      tag.span title, class: "inline-flex items-center rounded-md px-2.5 py-0.5 text-sm font-medium bg-#{@color}-100 text-#{@color}-800"
     end
   end
 end
