0846625a903db7b6e8bced4eb86ccb65aad29c2a
Atique Akhtar <atique@kiprosh.com>
2022-09-19 01:49:03 +0530

    Add trapModalFocus in accessibility controller

--- app/javascript/controllers/accessibility_controller.js
+++ app/javascript/controllers/accessibility_controller.js
@@ -10,4 +10,37 @@ export default class extends Controller {
 
     element.setAttribute("aria-expanded", toggledVal)
   }
+
+  trapModalFocus() {
+    // add all the elements inside modal which are focusable
+    const focusableElements =
+      'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+    const modal = document.querySelector("#mobileFilterModal")
+
+    const firstFocusableElement = modal.querySelectorAll(focusableElements)[0]
+    const focusableContent = modal.querySelectorAll(focusableElements)
+    const lastFocusableElement = focusableContent[focusableContent.length - 1]
+
+    document.addEventListener("keydown", function (e) {
+      let isTabPressed = e.key === "Tab"
+
+      if (!isTabPressed) {
+        return
+      }
+
+      if (e.shiftKey) {
+        if (document.activeElement === firstFocusableElement) {
+          lastFocusableElement.focus()
+          e.preventDefault()
+        }
+      } else {
+        if (document.activeElement === lastFocusableElement) {
+          firstFocusableElement.focus()
+          e.preventDefault()
+        }
+      }
+    })
+
+    firstFocusableElement.focus()
+  }
 }
