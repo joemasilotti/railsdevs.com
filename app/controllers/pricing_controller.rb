67d8b4fd4fcb44f3db2b8d4002e2dc84e8562c84
Ferry Phang <ferry.pangestu@ymail.com>
2022-09-20 08:23:05 +0700

    Remove unused v1 & v2  pricing view

--- app/controllers/pricing_controller.rb
+++ app/controllers/pricing_controller.rb
@@ -2,12 +2,6 @@ class PricingController < ApplicationController
   def show
     @stats = Pricing::Stats.new
     @testimonials = Testimonial.all
-    render template
   end
-
-  private
-
-  def template
-    "pricing/v3/show"
-  end
+  
 end
