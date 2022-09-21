fd0e374159310b83c84ea333f834679685def111
Joe Masilotti <joe@masilotti.com>
2022-09-21 06:42:48 -0700

    Code formatting

--- app/javascript/src/clipboard.js
+++ app/javascript/src/clipboard.js
@@ -1,24 +1,18 @@
 class Clipboard {
-  static copy({ text, html }) {
-    console.log(text);
-    console.log(html);
-    const data = [
-      new ClipboardItem({
+  static copy({text, html}) {
+    const clipboardItem = new ClipboardItem({
         ["text/plain"]: new Blob([text], { type: "text/plain" }),
         ["text/html"]: new Blob([html], { type: "text/html" }),
-      }),
-    ];
+      })
 
-    navigator.clipboard.write(data).then(
-      () => {
-        /* Success */
-      },
+    navigator.clipboard.write([clipboardItem]).then(
+      () => { /* Success */ },
       (error) => {
         console.error(error);
-        alert("Failed to copy to clipboard.");
+        alert("Failed to copy to clipboard.")
       }
-    );
+    )
   }
 }
 
-export default Clipboard;
+export default Clipboard
