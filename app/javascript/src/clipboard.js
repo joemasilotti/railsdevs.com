3faedb8a88aede23341b99d5ed2a3f45fbbe2ad1
Ferry Phang <ferry.pangestu@ymail.com>
2022-09-21 11:04:04 +0700

    Fix clipboard bug

--- app/javascript/src/clipboard.js
+++ app/javascript/src/clipboard.js
@@ -1,22 +1,24 @@
 class Clipboard {
-  static copy({text, html}) {
+  static copy({ text, html }) {
+    console.log(text);
+    console.log(html);
     const data = [
-      this.clipboardItem({type: "text/plain", content: text}),
-      this.clipboardItem({type: "text/html", content: html})
-    ]
+      new ClipboardItem({
+        ["text/plain"]: new Blob([text], { type: "text/plain" }),
+        ["text/html"]: new Blob([html], { type: "text/html" }),
+      }),
+    ];
 
     navigator.clipboard.write(data).then(
-      () => { /* Success */ },
+      () => {
+        /* Success */
+      },
       (error) => {
-        console.error(error)
-        alert("Failed to copy to clipboard.")
+        console.error(error);
+        alert("Failed to copy to clipboard.");
       }
-    )
-  }
-
-  static clipboardItem({type, content}) {
-    return new ClipboardItem({[type]: new Blob([content], {type})})
+    );
   }
 }
 
-export default Clipboard
+export default Clipboard;
