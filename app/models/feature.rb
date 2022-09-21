f9da85798546d2701c07e99f673accfedc7097ee
Mansa Keïta <mansakondo22@gmail.com>
2022-09-21 21:57:12 +0200

    Hide page 2+ of search results for non-subscribers (#661)
    * Hide page 2+ of search results for non-subscribers
    
    * TEMP: Seed 100 developers to show pagination bug
    
    * Improve the code
    
    * Design and copy for CTA
    
    * Revert "TEMP: Seed 100 developers to show pagination bug"
    
    This reverts commit ed469735901d1a89ffc22df10905ae6a49ced983.
    
    * Move developer-focused tests out of scroll tests
    
    * Test the new behaviour of developer queries
    
    * Remove the `User#permissions` abstraction
    
    * Wrap the new `DeveloperQuery` behavior in a feature flag
    
    * Render the `SubscriptionCTAComponent` or the `EmptyStateComponent`
    
    * Show the subscription CTA only when the `:paywalled_results` feature flag is enabled
    
    * Update CHANGELOG [skip ci]
    
    Co-authored-by: Joe Masilotti <joe@masilotti.com>

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -3,6 +3,8 @@ class Feature
     case feature_name.to_sym
     when :obfuscate_developer_urls
       !Rails.env.production?
+    when :paywalled_search_results
+      !Rails.env.production?
     end
   end
 end
