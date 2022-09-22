8dbcc7bfb25c29c923cee8f8f07a946a80fbd5da
Facundo Espinosa <facundo.espinosa@fing.edu.uy>
2022-09-22 14:39:17 -0300

    Keep preview rendering on erb files

--- test/components/previews/admin/forms/container_component_preview.rb
+++ test/components/previews/admin/forms/container_component_preview.rb
@@ -1,18 +1,15 @@
 module Admin
   module Forms
     class ContainerComponentPreview < ViewComponent::Preview
-      # Default Form Container
-      # ---------------
-      #
       # @param title text
       # @param description textarea
       def default(title: "title", description: "description")
-        render Admin::Forms::ContainerComponent.new(title, description)
+        render_with_template(locals: {
+          title: title,
+          description: description
+        })
       end
 
-      # Form Container with aside
-      # ---------------
-      #
       # @param title text
       # @param description textarea
       def with_aside(title: "title", description: "description")
@@ -22,9 +19,6 @@ module Admin
         })
       end
 
-      # Form Container with button_group
-      # ---------------
-      #
       # @param title text
       # @param description textarea
       def with_button_group(title: "title", description: "description")
@@ -34,9 +28,6 @@ module Admin
         })
       end
 
-      # Form Container with content
-      # ---------------
-      #
       # @param title text
       # @param description textarea
       def with_content(title: "title", description: "description")
@@ -46,9 +37,7 @@ module Admin
         })
       end
 
-      # Form Container with content, aside and button_group
-      # ---------------
-      #
+      # @label with content, aside and button_group
       # @param title text
       # @param description textarea
       def with_content_and_aside_and_button_group(title: "title", description: "description")
--- test/components/previews/admin/tables/table_component_preview.rb
+++ test/components/previews/admin/tables/table_component_preview.rb
@@ -2,43 +2,14 @@ module Admin
   module Tables
     class TableComponentPreview < ViewComponent::Preview
       def default
-        render Admin::Tables::TableComponent.new do |table|
-          table.with_header "name"
-          table.with_header "github"
-          table.with_header "email"
-
-          table.with_row do |row|
-            row.with_cell(primary: true) { Faker::Name.name }
-            row.with_cell { Faker::Internet.username }
-            row.with_cell { Faker::Internet.email }
-          end
-
-          table.with_row do |row|
-            row.with_cell(primary: true) { Faker::Name.name }
-            row.with_cell { Faker::Internet.username }
-            row.with_cell { Faker::Internet.email }
-          end
-
-          table.with_row do |row|
-            row.with_cell(primary: true) { Faker::Name.name }
-            row.with_cell { Faker::Internet.username }
-            row.with_cell { Faker::Internet.email }
-          end
-        end
       end
 
       # @!group Header
 
       def default_header
-        render Admin::Tables::TableComponent.new do |table|
-          table.with_header "name"
-        end
       end
 
       def header_right_aligned
-        render Admin::Tables::TableComponent.new do |table|
-          table.with_header "name", align: :right
-        end
       end
 
       # @!endgroup
@@ -46,27 +17,12 @@ module Admin
       # @!group Cell
 
       def default_cell
-        render Admin::Tables::TableComponent.new do |table|
-          table.with_row do |row|
-            row.with_cell { Faker::Name.name }
-          end
-        end
       end
 
       def primary_cell
-        render Admin::Tables::TableComponent.new do |table|
-          table.with_row do |row|
-            row.with_cell(primary: true) { Faker::Name.name }
-          end
-        end
       end
 
       def cell_right_aligned
-        render Admin::Tables::TableComponent.new do |table|
-          table.with_row do |row|
-            row.with_cell(align: :right) { Faker::Name.name }
-          end
-        end
       end
 
       # @!endgroup
