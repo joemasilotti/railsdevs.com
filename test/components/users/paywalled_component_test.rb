2f5f86ee42b7a3774cc990b7dc29a61d39dc7aa4
Joe Masilotti <joe@masilotti.com>
2022-10-21 11:04:09 -0700

    Move VC authorization logic to Users::Permission

--- test/components/users/paywalled_component_test.rb
+++ test/components/users/paywalled_component_test.rb
@@ -2,70 +2,41 @@ require "test_helper"
 
 module Users
   class PaywalledComponentTest < ViewComponent::TestCase
-    test "should restrict paywall content" do
-      user = users(:business)
-      developer = developers(:one)
-
-      render_inline(PaywalledComponent.new(user:, paywalled: developer)) { "Test text" }
-      assert_no_text "Test text"
-
-      render_inline(PaywalledComponent.new(user: nil, paywalled: developer)) { "Test text" }
-      assert_no_text "Test text"
-
-      render_inline(PaywalledComponent.new(user: nil, paywalled: nil)) { "Test text" }
-      assert_no_text "Test text"
+    setup do
+      @user = users(:business)
+      @resource = developers(:one)
+      @content = "Paywalled content"
     end
 
-    test "should show paywall content to customers" do
+    test "restricts paywalled content" do
+      render_inline(PaywalledComponent.new(@user, @resource)) { @content }
+      assert_no_text @content
+    end
+
+    test "shows paywalled content when authorized" do
       user = users(:subscribed_business)
-      developer = developers(:one)
-      render_inline(PaywalledComponent.new(user:, paywalled: developer)) { "Test text" }
-
-      assert_text "Test text"
+      render_inline(PaywalledComponent.new(user, @resource)) { @content }
+      assert_text @content
     end
 
-    test "should show paywall content to the owner" do
-      user = users(:developer)
-      developer = developers(:one)
-      render_inline(PaywalledComponent.new(user:, paywalled: developer)) { "Test text" }
-
-      assert_text "Test text"
+    test "shows paywalled content with a valid public profile keys" do
+      public_key = @resource.public_profile_key
+      render_inline(PaywalledComponent.new(nil, @resource, public_key:)) { @content }
+      assert_text @content
     end
 
-    test "should show small CTA if paywalled" do
-      user = users(:business)
-      developer = developers(:one)
+    test "shows a small CTA" do
+      render_inline(PaywalledComponent.new(@user, @resource, size: :small)) { @content }
 
-      render_inline(PaywalledComponent.new(user:, paywalled: developer, size: :small)) { "Test text" }
       assert_text I18n.t("users.paywalled_component.title")
       assert_no_text I18n.t("users.paywalled_component.description")
     end
 
-    test "should show large CTA if paywalled" do
-      user = users(:business)
-      developer = developers(:one)
+    test "shows a large CTA" do
+      render_inline(PaywalledComponent.new(@user, @resource, size: :large)) { @content }
 
-      render_inline(PaywalledComponent.new(user:, paywalled: developer, size: :large)) { "Test text" }
       assert_text I18n.t("users.paywalled_component.title")
       assert_text I18n.t("users.paywalled_component.description")
-    end
-
-    test "should show paywall content if valid profile access key" do
-      user = users(:business)
-      developer = developers(:one)
-      developer.share_url
-      render_inline(PaywalledComponent.new(user:, paywalled: developer, size: :large, public_key: developer.public_profile_key)) { "Test text" }
-      assert_text "Test text"
-    end
-
-    test "should show small CTA if profile access key does not match" do
-      user = users(:business)
-      developer = developers(:one)
-      developer.share_url
-      render_inline(PaywalledComponent.new(user:, paywalled: developer, size: :large, public_key: "randomkey")) { "Test text" }
-      assert_text I18n.t("users.paywalled_component.title")
-      assert_text I18n.t("users.paywalled_component.description")
-      assert_no_text "Test text"
     end
   end
 end
