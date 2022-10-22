9880f188ec4a00d3a2c089cb4395a97dbc01e168
mansakondo <mansakondo22@gmail.com>
2022-10-22 15:23:33 +0200

    Initialize `Users::PaywalledComponent` objects correctly

--- app/components/users/paywalled/collapse_control_component.rb
+++ app/components/users/paywalled/collapse_control_component.rb
@@ -2,6 +2,6 @@
 
 class Users::Paywalled::CollapseControlComponent < CollapseControlComponent
   renders_one :cta, ->(user:) do
-    Users::PaywalledComponent.new(user: user, paywalled: nil, size: :extra_small)
+    Users::PaywalledComponent.new(user, nil, size: :extra_small)
   end
 end
