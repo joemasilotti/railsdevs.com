3bf972f6e5101bef179c6581b35c6a326950f5bc
Joe Masilotti <joe@masilotti.com>
2022-10-28 13:10:47 -0700

    Don't touch updated_at when setting this property (#719)
    Otherwise a developer might get the "Recently active" badge.

--- app/controllers/admin/developers/source_contributors_controller.rb
+++ app/controllers/admin/developers/source_contributors_controller.rb
@@ -2,13 +2,21 @@ module Admin
   module Developers
     class SourceContributorsController < ApplicationController
       def create
-        Developer.find(params[:developer_id]).update(source_contributor: true)
+        set_developer_source_contributor(true)
         redirect_back_or_to root_path, allow_other_host: false
       end
 
       def destroy
-        Developer.find(params[:developer_id]).update(source_contributor: false)
+        set_developer_source_contributor(false)
         redirect_back_or_to root_path, allow_other_host: false
+      end
+
+      private
+
+      def set_developer_source_contributor(source_contributor)
+        developer = Developer.find(params[:developer_id])
+        developer.source_contributor = source_contributor
+        developer.save!(touch: false)
       end
     end
   end
