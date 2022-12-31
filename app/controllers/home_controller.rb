fcc6b78c771f43680fc7d276e88681c796fce32c
Jonathan Bennett <jonathan@jbennett.me>
2022-12-31 09:47:20 -0500

    cache and randomize home page developers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,19 @@
 class HomeController < ApplicationController
   def show
-    @developers = Developer
-      .visible
-      .includes(:role_type).with_attached_avatar
-      .actively_looking.newest_first.available
-      .limit(10)
+    @developers = Developer.where(id: developer_ids)
+  end
+
+  private
+
+  def developer_ids
+    Rails.cache.fetch("home_show/developer_ids", expires_in: 1.day) do
+      Developer
+        .visible
+        .includes(:role_type).with_attached_avatar
+        .actively_looking.available
+        .order("RANDOM()")
+        .limit(10)
+        .pluck(:id)
+    end
   end
 end
