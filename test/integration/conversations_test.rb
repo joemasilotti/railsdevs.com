f751ca5ded7d687966d72433a5eba53ee2e87d90
Troy Underwood <troyizzle@gmail.com>
2023-01-26 15:52:05 -0600

    fix title

--- test/integration/conversations_test.rb
+++ test/integration/conversations_test.rb
@@ -94,5 +94,6 @@ class ConversationsTest < ActionDispatch::IntegrationTest
 
     assert_match(/deleted account/, response.body)
     assert_no_match(/Pay hiring fee/, response.body)
+    assert_select "title", "Conversation with (deleted account) · RailsDevs"
   end
 end
