6f23ca1ab91e8f56d963128d5284ca5bd8cad909
Joe Masilotti <joe@masilotti.com>
2023-02-18 11:45:42 -0800

    Ensure mailer preview always show referred biz/dev

--- db/seeds/referrals.rb
+++ db/seeds/referrals.rb
@@ -1,15 +1,12 @@
-User.first.update referral_code: "abc123"
+referral_code = "abc123"
+
+referring_user = User.find_by(email: "admin@example.com")
+referring_user.update!(referral_code:) unless referring_user.referral_code == referral_code
 
 # Business referral
-Referral.create(
-  referred_user: User.find_by(email: "business@example.com"),
-  referring_user: User.first,
-  code: "abc123"
-)
+referred_user = User.find_by(email: "business@example.com")
+SeedsHelper.create_referral!(referred_user:, referring_user:, code: referral_code)
 
 # Developer referral
-Referral.create(
-  referred_user: User.find_by(email: "zona@example.com"),
-  referring_user: User.first,
-  code: "abc123"
-)
+referred_user = User.find_by(email: "developer@example.com")
+SeedsHelper.create_referral!(referred_user:, referring_user:, code: referral_code)
