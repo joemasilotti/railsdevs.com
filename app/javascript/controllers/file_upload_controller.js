25f1426f01b91dd3be1dcd34fc239859b7aec40d
Ryo Naruse <naruse19910801@gmail.com>
2023-02-27 08:12:42 +0900

    fix: change src in picture queries

--- app/javascript/controllers/file_upload_controller.js
+++ app/javascript/controllers/file_upload_controller.js
@@ -6,8 +6,17 @@ export default class extends Controller {
 
   select(event) {
     const file = event.currentTarget.files[0]
-    this.imageTarget.src = window.URL.createObjectURL(file)
-    this.imageTarget.srcset = window.URL.createObjectURL(file)
+    const objectUrl = window.URL.createObjectURL(file)
+    this.imageTarget.src = objectUrl
+    this.imageTarget.srcset = objectUrl
+    // NOTE: Update the srcset of the <source> tag of the <picture> tag sub-element
+    const picture = event.currentTarget.closest('div').querySelector('picture');
+    if(picture) {
+      const sources = picture.querySelectorAll('source')
+      sources.forEach(source => {
+        source.srcset = objectUrl
+      });
+    }
     this.imageTarget.classList.remove(this.visibilityClass)
     this.errorTarget.classList.add(this.visibilityClass)
   }
