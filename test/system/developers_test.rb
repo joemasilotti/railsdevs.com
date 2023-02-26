452360e6d5ea72a7b6156a21ecf0726352f6668c
Ryo Naruse <naruse19910801@gmail.com>
2023-02-27 08:13:36 +0900

    chore: remove unnecessary file

--- test/system/developers_test.rb
+++
@@ -1,7 +0,0 @@
-require "application_system_test_case"
-
-class DevelopersSpec < ApplicationSystemTestCase
-  test "developers availability sort click adds sort param", js: true do
-    visit developers_path
-  end
-end
