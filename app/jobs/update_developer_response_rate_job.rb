ab734b2157fec7d8aebff64f2ec25f4a6c1cef7f
fig <fig@users.noreply.github.com>
2023-02-27 17:26:06 +0000

    Add a note to change persistance method later.
    We are using update_column to avoid changing the updated_at
    timestamp on the developer record which would trigger the rendering of a
    'Recently Active' badge on the developer profile.
    This should be changed to update! once we have implemented cacheing of 
    the
    badges in their own model.

--- app/jobs/update_developer_response_rate_job.rb
+++ app/jobs/update_developer_response_rate_job.rb
@@ -6,6 +6,11 @@ class UpdateDeveloperResponseRateJob < ApplicationJob
   def perform(developer)
     conversations = developer.conversations.where("created_at < ?", GRACE_PERIOD.ago)
     replied_rate = Stats::Conversation.new(conversations).replied_rate
+    # TODO: (Fig) - We are using update_column to avoid changing the updated_at
+    # timestamp on the developer record which would trigger the rendering of a
+    # 'Recently Active' badge on the developer profile.
+    # This should be changed to update! once we have implemented cacheing of the
+    # badges in their own model.
     developer.update_column(:response_rate, (replied_rate * 100).round)
   end
 end
