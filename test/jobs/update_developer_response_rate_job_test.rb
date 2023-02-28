cb1cedecd2ae14a66c5b8ab1d585f44576f09627
fig <fig@users.noreply.github.com>
2023-02-28 23:53:25 +0000

    Tidy up tests

--- test/jobs/update_developer_response_rate_job_test.rb
+++ test/jobs/update_developer_response_rate_job_test.rb
@@ -2,80 +2,62 @@ require "test_helper"
 
 class UpdateDeveloperResponseRateJobTest < ActiveJob::TestCase
   include BusinessesHelper
+
+  GRACE_PERIOD = UpdateDeveloperResponseRateJob::GRACE_PERIOD
+
   setup do
     @developer = developers(:one)
-
-    # Create a conversation that has been replied to.
-    business = Business.create!(business_attributes)
-    conversation = Conversation.create!(
-      developer: @developer,
-      business: business,
-      created_at: UpdateDeveloperResponseRateJob::GRACE_PERIOD.ago - 1.day
-    )
-    Message.create!(conversation: conversation, sender: business, body: "First contact")
-    Message.create!(conversation: conversation, sender: @developer, body: "First reply")
-
-    # Create a conversation that has not been replied to.
-    business = Business.create!(business_attributes)
-    conversation = Conversation.create!(
-      developer: @developer,
-      business: business,
-      created_at: UpdateDeveloperResponseRateJob::GRACE_PERIOD.ago - 1.day
-    )
-    Message.create!(conversation: conversation, sender: business, body: "Second contact")
-
-    # update the developer's response rate
-    UpdateDeveloperResponseRateJob.perform_now(@developer.reload)
+    create_answered_message_for(@developer)
+    create_ignored_message_for(@developer, grace_period_expired: true)
+    UpdateDeveloperResponseRateJob.perform_now(@developer)
   end
 
-  test "should update developer response rate from the returned value" do
-    replied_rate = 0.42
-    expected_response_rate = 42
-    mock = Minitest::Mock.new
-    mock.expect :replied_rate, replied_rate
+  test "should update developer response" do
+    UpdateDeveloperResponseRateJob.perform_now(@developer)
 
-    Stats::Conversation.stub :new, mock do
-      UpdateDeveloperResponseRateJob.perform_now(@developer.reload)
-      assert_equal expected_response_rate, @developer.reload.response_rate
-    end
-    assert_mock mock
+    assert_equal 50, @developer.reload.response_rate
   end
 
   test "should not take into account unreplied conversations created during grace period" do
-    before = @developer.reload.response_rate
+    assert_no_changes @developer.reload.response_rate do
+      create_ignored_message_for(@developer, grace_period_expired: false)
 
-    business = Business.create!(business_attributes)
-    conversation = Conversation.create!(developer: @developer, business: business)
-    Message.create!(conversation: conversation, sender: business, body: "First contact")
-    UpdateDeveloperResponseRateJob.perform_now(@developer.reload)
-
-    assert_equal @developer.reload.response_rate, before
+      UpdateDeveloperResponseRateJob.perform_now(@developer)
+    end
   end
 
   test "should increase response rate if replied to within grace period" do
-    before = @developer.reload.response_rate
+    assert_changes -> { @developer.reload.response_rate }, from: 50, to: 67 do
+      create_answered_message_for(@developer, grace_period_expired: false)
 
-    business = Business.create!(business_attributes)
-    conversation = Conversation.create!(developer: @developer, business: business)
-    Message.create!(conversation: conversation, sender: business, body: "First contact")
-    Message.create!(conversation: conversation, sender: @developer, body: "First reply")
-    UpdateDeveloperResponseRateJob.perform_now(@developer.reload)
-
-    assert_operator @developer.reload.response_rate, :>, before
+      UpdateDeveloperResponseRateJob.perform_now(@developer)
+    end
   end
 
   test "should reduce response rate if not replied to beyond grace period" do
-    before = @developer.response_rate
+    assert_changes -> { @developer.reload.response_rate }, from: 50, to: 33 do
+      create_ignored_message_for(@developer, grace_period_expired: true)
 
+      UpdateDeveloperResponseRateJob.perform_now(@developer)
+    end
+  end
+
+  private
+
+  def create_answered_message_for(developer, grace_period_expired: false)
+    conversation = start_conversation(developer, grace_period_expired)
+    Message.create!(conversation:, sender: developer, body: "First reply")
+  end
+
+  def create_ignored_message_for(developer, grace_period_expired: false)
+    start_conversation(developer, grace_period_expired)
+  end
+
+  def start_conversation(developer, grace_period_expired)
     business = Business.create!(business_attributes)
-    conversation = Conversation.create!(
-      developer: @developer,
-      business: business,
-      created_at: UpdateDeveloperResponseRateJob::GRACE_PERIOD.ago - 1.hour
-    )
-    Message.create!(conversation: conversation, sender: business, body: "First contact")
-
-    UpdateDeveloperResponseRateJob.perform_now(@developer.reload)
-    assert_operator @developer.reload.response_rate, :<, before
+    conversation = Conversation.create!(developer:, business:)
+    conversation.update!(created_at: GRACE_PERIOD.ago - 1.hour) if grace_period_expired
+    Message.create!(conversation:, sender: business, body: "First contact")
+    conversation
   end
 end
