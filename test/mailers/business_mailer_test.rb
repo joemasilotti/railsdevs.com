1d770fcaff5941f5baf44edb0711b54abf4ad555
Duncan Miller <dmiller@heatspring.com>
2023-03-02 11:38:46 -0800

    adding assertion to business mailer subscribed test

--- test/mailers/business_mailer_test.rb
+++ test/mailers/business_mailer_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class BusinessMailerTest < ActionMailer::TestCase
   test "subscribed" do
-    BusinessMailer.with(business:).subscribed.deliver
+    BusinessMailer.with(business:).subscribed.deliver_later
+    assert_enqueued_emails 1
   end
 
   def business
