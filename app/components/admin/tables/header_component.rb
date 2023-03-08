07c108b91496532d156c83b99ae82536c5493e3d
franlocus <franciscobuscema@gmail.com>
2023-03-08 13:01:28 -0300

    Restore deleted blank line; Move the title keyword so is on the same line as scope

--- app/components/admin/tables/header_component.rb
+++ app/components/admin/tables/header_component.rb
@@ -12,9 +12,8 @@ module Admin
       end
 
       def call
-        tag.th title || content, scope: "col",
-          class: class_names("px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider", align_class),
-          title: tooltip
+        tag.th title || content, scope: "col", title: tooltip,
+          class: class_names("px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider", align_class)
       end
     end
   end
