294a172664e28ffefc335f898ad5e261796ccf1f
franlocus <franciscobuscema@gmail.com>
2023-03-07 23:03:38 -0300

    add tooltip with html title to headers on the admin conversations dashboard

--- app/components/admin/tables/header_component.rb
+++ app/components/admin/tables/header_component.rb
@@ -3,16 +3,18 @@ module Admin
     class HeaderComponent < ApplicationComponent
       include CellAlignment
 
-      attr_reader :title, :align
+      attr_reader :title, :tooltip, :align
 
-      def initialize(title = nil, align: :left)
+      def initialize(title = nil, tooltip = nil, align: :left)
         @title = title
+        @tooltip = tooltip
         @align = align
       end
 
       def call
         tag.th title || content, scope: "col",
-          class: class_names("px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider", align_class)
+          class: class_names("px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider", align_class),
+          title: tooltip
       end
     end
   end
