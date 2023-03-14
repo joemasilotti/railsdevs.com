a33fc5b2ce800a20a338eca77f9cef12cadcb53c
zzJZzz <zzjzzz.creator@gmail.com>
2023-03-14 11:38:36 -0400

    Add end

--- test/jobs/disabled_email_job_test.rb
+++ test/jobs/disabled_email_job_test.rb
@@ -1,24 +1,24 @@
-require 'test_helper'
+require "test_helper"
 
 class DisabledEmailJobTest < ActiveJob::TestCase
- include NotificationsHelper
+  include NotificationsHelper
   test "discards job on Postmark::InactiveRecipientError exception" do
-  email_payload = payload.merge("Recipient" => "invalid-recipient@example.com")
+    email_payload = payload.merge("Recipient" => "invalid-recipient@example.com")
 
-  custom_logger = StringIO.new
-  Rails.logger = Logger.new(custom_logger)
+    custom_logger = StringIO.new
+    Rails.logger = Logger.new(custom_logger)
 
-  assert_no_difference "InboundEmail.count" do
-    assert_raises(Postmark::InactiveRecipientError) do
-      InboundEmailJob.perform_now(email_payload)
+    assert_no_difference "InboundEmail.count" do
+      assert_raises(Postmark::InactiveRecipientError) do
+        InboundEmailJob.perform_now(email_payload)
+      end
+    end
+
+    logged_message = custom_logger.string
+    assert_match(/Inactive recipient error: /, logged_message)
+
+    assert_difference "InboundEmail.count", 1 do
+      InboundEmailJob.perform_now(payload)
     end
   end
-
-  logged_message = custom_logger.string
-  assert_match(/Inactive recipient error: /, logged_message)
-
-  assert_difference "InboundEmail.count", 1 do
-    InboundEmailJob.perform_now(payload)
-  end
-end
 end
