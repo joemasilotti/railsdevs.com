56c4c12ea3cb5a3151e13fc6fbb3a3c7c22b9863
Joe Masilotti <joe@masilotti.com>
2023-03-15 08:29:59 -0700

    Use Rails.root.join directly

--- db/seeds/specialties.rb
+++ db/seeds/specialties.rb
@@ -1,4 +1,4 @@
-specialties = YAML.load_file(File.join(Rails.root, "db", "seeds", "specialties.yml"))
+specialties = YAML.load_file(Rails.root.join("db", "seeds", "specialties.yml"))
 specialties.each do |specialty|
   Specialty.find_or_create_by!(name: specialty)
 end
