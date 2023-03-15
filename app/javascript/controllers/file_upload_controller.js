1f6a385709c24232cd477f72b5546ad52b6e64ad
Joe Masilotti <joe@masilotti.com>
2023-03-15 08:49:43 -0700

    Convert CSS query to Stimulus target

--- app/javascript/controllers/file_upload_controller.js
+++ app/javascript/controllers/file_upload_controller.js
@@ -1,22 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["activity", "image", "error"]
+  static targets = ["activity", "image", "source", "error"]
   static classes = ["visibility", "loading"]
 
   select(event) {
     const file = event.currentTarget.files[0]
     const objectUrl = window.URL.createObjectURL(file)
+
     this.imageTarget.src = objectUrl
     this.imageTarget.srcset = objectUrl
-    // NOTE: Update the srcset of the <source> tag of the <picture> tag sub-element
-    const picture = event.currentTarget.closest('div').querySelector('picture');
-    if(picture) {
-      const sources = picture.querySelectorAll('source')
-      sources.forEach(source => {
-        source.srcset = objectUrl
-      });
-    }
+    this.sourceTargets.forEach(source => {
+      source.srcset = objectUrl
+    })
+
     this.imageTarget.classList.remove(this.visibilityClass)
     this.errorTarget.classList.add(this.visibilityClass)
   }
