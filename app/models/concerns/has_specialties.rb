fdf786ed1fe52b01646bbe9d31a3cac798f5886f
Joe Masilotti <joe@masilotti.com>
2023-03-15 13:53:20 -0700

    Limit specialties to 5

--- app/models/concerns/has_specialties.rb
+++ app/models/concerns/has_specialties.rb
@@ -4,5 +4,7 @@ module HasSpecialties
   included do
     has_many :specialty_tags
     has_many :specialties, -> { visible }, through: :specialty_tags, dependent: :destroy
+
+    validates :specialties, length: {maximum: 5}
   end
 end
