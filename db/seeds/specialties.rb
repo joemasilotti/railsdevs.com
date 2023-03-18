582e4331710b665e110f38bf746c573bc5a67b12
Stephen Margheim <stephen.margheim@gmail.com>
2023-03-18 21:36:56 +0100

    Use upsert_all in Specialities seeds

--- db/seeds/specialties.rb
+++ db/seeds/specialties.rb
@@ -1,7 +1,7 @@
-specialties = YAML.load_file(Rails.root.join("db", "seeds", "specialties.yml"))
-specialties.each do |specialty|
-  Specialty.find_or_create_by!(name: specialty)
-end
+Specialty.upsert_all(
+  YAML.load_file(Rails.root.join("db", "seeds", "specialties.yml")),
+  unique_by: :name
+)
 
 User.find_by(email: "developer@example.com").developer.specialties = Specialty.first(5)
 User.find_by(email: "freelancer@example.com").developer.specialties = [Specialty.second, Specialty.third]
