b8ab79165452088f5f559ae0f07ad783e33cca30
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-03-18 19:39:59 +0530

    fix typo

--- db/migrate/20230318042319_add_archived_attributes_to_conversation.rb
+++ db/migrate/20230318042319_add_archived_attributes_to_conversation.rb
@@ -1,4 +1,4 @@
-class AddArchivedAtrributesToConversation < ActiveRecord::Migration[7.0]
+class AddArchivedAttributesToConversation < ActiveRecord::Migration[7.0]
   def change
     add_column :conversations, :developer_archived_at, :datetime
     add_column :conversations, :business_archived_at, :datetime
