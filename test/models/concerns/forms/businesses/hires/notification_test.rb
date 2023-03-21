f09d2c3ec099818723bd5a229087f5d88b5340b5
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2023-03-21 13:44:44 -0700

    * add new line module syntax to hires/notification_test.rb
    * restore newline to user_component.html.erb

--- test/models/concerns/forms/businesses/hires/notification_test.rb
+++ test/models/concerns/forms/businesses/hires/notification_test.rb
@@ -1,21 +1,25 @@
 require "test_helper"
 
-class Forms::Businesses::Hires::Notifications::NotificationsTest < ActiveSupport::TestCase
-  include ActionMailer::TestHelper
-  include Forms::Businesses::HiresHelper
-  include NotificationsHelper
+module Forms
+  module Businesses
+    class Hires::Notifications::NotificationsTest < ActiveSupport::TestCase
+      include ActionMailer::TestHelper
+      include Forms::Businesses::HiresHelper
+      include NotificationsHelper
 
-  test "sends a notification to the admins" do
-    hire_form = Forms::Businesses::Hire.new(form_attributes)
-    assert_sends_notification Admin::Forms::Businesses::HireNotification, to: users(:admin) do
-      assert hire_form.save_and_notify
-    end
-  end
+      test "sends a notification to the admins" do
+        hire_form = Forms::Businesses::Hire.new(form_attributes)
+        assert_sends_notification Admin::Forms::Businesses::HireNotification, to: users(:admin) do
+          assert hire_form.save_and_notify
+        end
+      end
 
-  test "invalid records don't send notifications" do
-    hire_form = Forms::Businesses::Hire.new
-    refute_sends_notifications do
-      refute hire_form.save_and_notify
+      test "invalid records don't send notifications" do
+        hire_form = Forms::Businesses::Hire.new
+        refute_sends_notifications do
+          refute hire_form.save_and_notify
+        end
+      end
     end
   end
 end
