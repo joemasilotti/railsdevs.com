9e31a59847f16e1259d404b1acdb2ed53ef4bef5
Mike Ray <6802326+MikeRayUX@users.noreply.github.com>
2023-03-22 20:12:18 -0700

    Update test/models/concerns/businesses/hiring_invoice_requests/notifications_test.rb
    Co-authored-by: Joe Masilotti <joe@masilotti.com>

--- test/models/concerns/businesses/hiring_invoice_requests/notifications_test.rb
+++ test/models/concerns/businesses/hiring_invoice_requests/notifications_test.rb
@@ -2,20 +2,20 @@ require "test_helper"
 
 module Businesses
   module HiringInvoiceRequests
-    class Notification::NotificationsTest < ActiveSupport::TestCase
+    class NotificationsTest < ActiveSupport::TestCase
       include ActionMailer::TestHelper
-      include Businesses::HiringInvoiceRequestsHelper
+      include HiringInvoiceRequestsHelper
       include NotificationsHelper
 
       test "sends a notification to the admins" do
-        hire_form = Businesses::HiringInvoiceRequest.new(form_attributes)
+        hire_form = HiringInvoiceRequest.new(form_attributes)
         assert_sends_notification Admin::Businesses::HiringInvoiceRequestNotification, to: users(:admin) do
           assert hire_form.save_and_notify
         end
       end
 
       test "invalid records don't send notifications" do
-        hire_form = Businesses::HiringInvoiceRequest.new
+        hire_form = HiringInvoiceRequest.new
         refute_sends_notifications do
           refute hire_form.save_and_notify
         end
