2b6491baf0caef7a34e68853d168cae99926bc84
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2023-03-24 17:13:42 -0700

    restore devleoper namespace to model instantiation

--- app/controllers/developers/celebration_package_requests_controller.rb
+++ app/controllers/developers/celebration_package_requests_controller.rb
@@ -1,15 +1,14 @@
 module Developers
   class CelebrationPackageRequestsController < ApplicationController
-    before_action :test
     before_action :authenticate_user!
     before_action :require_developer!
 
     def new
-      @form = ::CelebrationPackageRequest.new(developer: current_user.developer)
+      @form = Developers::CelebrationPackageRequest.new(developer: current_user.developer)
     end
 
     def create
-      @form = ::CelebrationPackageRequest.new(developers_celebration_package_request_params)
+      @form = Developers::CelebrationPackageRequest.new(developers_celebration_package_request_params)
       if @form.save_and_notify
         redirect_to root_path, notice: t(".success")
       else
