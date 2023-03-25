9cf0ce13394fccf9b7d041e82417ba41d3d6c9b8
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2023-03-24 17:00:19 -0700

    rename form param to match new celebration_request model

--- app/notifications/admin/developers/new_celebration_package_request_notification.rb
+++ app/notifications/admin/developers/new_celebration_package_request_notification.rb
@@ -4,18 +4,18 @@ module Admin
       deliver_by :database
       deliver_by :email, mailer: "AdminMailer", method: :developers_celebration_package_request
 
-      param :form
+      param :celebration_package_request
 
       def title
-        t("notifications.admin.developers.celebration_package_request.title", developer: form.developer.name)
+        t("notifications.admin.developers.celebration_package_request.title", developer: celebration_package_request.developer.name)
       end
 
       def url
-        admin_developers_celebration_package_requests_path(form)
+        admin_developers_celebration_package_requests_path(celebration_package_request)
       end
 
-      def form
-        params[:form]
+      def celebration_package_request
+        params[:celebration_package_request]
       end
     end
   end
