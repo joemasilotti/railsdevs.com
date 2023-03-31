68d39954b85564e6e60cbb5b7daa042dedcb3adf
Joe Masilotti <joe@masilotti.com>
2023-03-31 14:16:35 -0700

    Rework test to use counter cache

--- test/integration/admin/referrals_test.rb
+++ test/integration/admin/referrals_test.rb
@@ -1,20 +1,21 @@
 require "test_helper"
 
-class ReferralsTest < ActionDispatch::IntegrationTest
-  test "must be signed in" do
-    get admin_referrals_path
-    assert_redirected_to new_user_session_path
-  end
+module Admin
+  class ReferralsTest < ActionDispatch::IntegrationTest
+    test "must be an admin" do
+      sign_in users(:empty)
+      get admin_referrals_path
+      assert_redirected_to root_path
+    end
 
-  test "must be an admin" do
-    sign_in users(:empty)
-    get admin_referrals_path
-    assert_redirected_to root_path
-  end
+    test "list all users referrals" do
+      user = users(:admin)
+      User.reset_counters(user.id, :referrals)
+      assert_equal 1, user.reload.referrals_count
 
-  test "list all users referrals" do
-    sign_in users(:admin)
-    get admin_referrals_path
-    assert_select "tr td", text: users(:admin).email
+      sign_in user
+      get admin_referrals_path
+      assert_select "td", text: "1"
+    end
   end
 end
