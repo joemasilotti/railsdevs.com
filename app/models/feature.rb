584344bc60003216175a4ecf5159764d0ed1883d
Joe Masilotti <joe@masilotti.com>
2023-04-02 09:56:23 -0700

    Enable cancellation confirmation email

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -4,7 +4,7 @@ class Feature
     when :badge_filter
       !Rails.env.production?
     when :cancel_subscription
-      !Rails.env.production?
+      true
     when :redesign
       ENV.fetch("REDESIGN", false)
     when :business_welcome_email
