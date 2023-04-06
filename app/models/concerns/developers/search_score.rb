7b157765cec8c323e52f3a20cad2bc2f995a65d3
Kasper Timm Hansen <kaspth@gmail.com>
2023-04-06 21:13:23 +0200

    check please

--- app/models/concerns/developers/search_score.rb
+++ app/models/concerns/developers/search_score.rb
@@ -2,21 +2,21 @@ module Developers::SearchScore
   extend ActiveSupport::Concern
 
   included do
-    small, medium, large, x_large = 5, 10, 20, 30
+    _small, medium, large, x_large = 5, 10, 20, 30
 
-    scores :scheduling_link?,    by: medium,  if: :present?
-    scores :source_contributor?, by: large,   if: :present?
-    scores :recently_added?,     by: x_large, if: :present?
+    scores :scheduling_link?, by: medium, if: :present?
+    scores :source_contributor?, by: large, if: :present?
+    scores :recently_added?, by: x_large, if: :present?
 
-    scores :bio, by: medium, if: -> { _1&.length > 500 }
-    scores :bio, by: -large, if: -> { _1&.length < 50 }
+    scores :bio, by: medium, if: -> { _1.length > 500 if _1 }
+    scores :bio, by: -large, if: -> { _1.length < 50 if _1 }
 
     scores :profile_updated_at, by: -medium, if: -> { _1&.before? 6.months.ago }
-    scores :profile_updated_at, by: medium,  if: -> { _1 && (3.months.ago..1.month.ago).cover?(_1) }
-    scores :profile_updated_at, by: large,   if: -> { _1&.after? 1.month.ago }
+    scores :profile_updated_at, by: medium, if: -> { _1 && (3.months.ago..1.month.ago).cover?(_1) }
+    scores :profile_updated_at, by: large, if: -> { _1&.after? 1.month.ago }
 
-    scores :response_rate, by: large,  if: -> { conversations_count.positive? && _1 >= HasBadges::HIGH_RESPONSE_RATE_CUTTOFF }
-    scores :response_rate, by: -large, if: -> { conversations_count.positive? && _1 <= HasBadges::LOW_RESPONSE_RATE_CUTTOFF  }
+    scores :response_rate, by: large, if: -> { conversations_count.positive? && _1 >= HasBadges::HIGH_RESPONSE_RATE_CUTTOFF }
+    scores :response_rate, by: -large, if: -> { conversations_count.positive? && _1 <= HasBadges::LOW_RESPONSE_RATE_CUTTOFF }
 
     before_save :update_search_score
   end
@@ -25,7 +25,7 @@ module Developers::SearchScore
     attr_reader :scorings
 
     def scores(attribute, by:, **options)
-      @scorings ||= Hash.new { |h,k| h[k] = [] }
+      @scorings ||= Hash.new { |h, k| h[k] = [] }
       @scorings[attribute] << -> { by if instance_exec(public_send(attribute), &options.fetch(:if)) }
     end
   end
--- test/models/concerns/developers/search_score_test.rb
+++ test/models/concerns/developers/search_score_test.rb
@@ -20,7 +20,7 @@ module Developers
     test "large demotion for low response rate" do
       @developer.response_rate = HasBadges::LOW_RESPONSE_RATE_CUTTOFF
       @developer.conversations_count = 1
-      assert_equal -20, @developer.score_for(:response_rate)
+      assert_equal(-20, @developer.score_for(:response_rate))
     end
 
     test "no change when no conversations" do
@@ -56,7 +56,7 @@ module Developers
 
     test "medium demotion for profile updated more than 6 months ago" do
       @developer.profile_updated_at = 7.months.ago
-      assert_equal -10, @developer.score_for(:profile_updated_at)
+      assert_equal(-10, @developer.score_for(:profile_updated_at))
     end
 
     test "extra large boost for profiles added in the last week" do
@@ -71,7 +71,7 @@ module Developers
 
     test "large demotion for bios with fewer than 50 characters" do
       @developer.bio = "X" * 49
-      assert_equal -20, @developer.score_for(:bio)
+      assert_equal(-20, @developer.score_for(:bio))
     end
   end
 end
