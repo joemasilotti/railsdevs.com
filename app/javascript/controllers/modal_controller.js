5c685e5ecfd4c41c5a2121835448d2a8f8dba572
Mads Ohm Larsen <mads.ohm@gmail.com>
2023-05-12 15:38:59 +0200

    Dismissible modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -4,9 +4,27 @@ export default class extends Controller {
   static targets = ["modal"]
   static values = {toggleTimeout: {type: Number, default: 1000}}
 
+  closeOnEsc(event) {
+    if (event.key === "Escape") {
+      this.close()
+    }
+  }
+
+  connect() {
+    document.addEventListener("keydown", this.closeOnEsc)
+  }
+
+  disconnect() {
+    document.removeEventListener("keydown", this.closeOnEsc)
+  }
+
   hideModal() {
     setTimeout(() => {
-      this.modalTarget.remove()
+      this.close()
     }, this.toggleTimeoutValue)
+  }
+
+  close() {
+    this.modalTarget.remove()
   }
 }
