e6165f8366ae30ab960870bb12dcb8baad5e2246
Mia Sinek <mia@miasinek.com>
2023-05-18 12:18:20 +0200

    Refactor: dropdown_pill_controller

--- app/javascript/controllers/dropdown_pill_controller.js
+++ app/javascript/controllers/dropdown_pill_controller.js
@@ -4,46 +4,62 @@ export default class extends Controller {
   static targets = ["dropdown", "input", "pillContainer"];
 
   connect() {
-    this.addPIllsFromCheckboxes()
-  }
-
-  addPIllsFromCheckboxes() {
-    // look up all checked checkboxes and add a pill for each
-    const checkedBoxes = document.querySelectorAll("div[id=specialties-accordion] input[type=checkbox]:checked")
-
-    checkedBoxes.forEach((checkbox) => {
-      this.addPill(checkbox.value)
-    })
-  }
-
-  addPill(domId) {
-    const displayedPill = document.getElementById(`specialty_${domId}`)
-
-    if (!displayedPill) {
-      const template = document.getElementById(`specialty_${domId}-template`)
-      const content = template.content.cloneNode(true)
-      this.pillContainerTarget.appendChild(content)
-
-      const checkbox = document.getElementById(`specialty_ids_${domId}`)
-      checkbox.checked = true
-    }
-    else {
-      // the pill is already there, so don't do anything
-    }
+    this.populatePillsFromCheckedCheckboxes()
   }
 
   removePill(event) {
     event.currentTarget.remove()
+    this.uncheckCheckbox(event.currentTarget.dataset.specialtyId)
+  }
 
-    const checkbox = document.getElementById(`specialty_ids_${event.currentTarget.dataset.specialtyId}`)
+  handleSpecialitySelection(event) {
+    event.preventDefault()
+
+    this.addPill(event.currentTarget.dataset.value)
+    this.clearInputAndDropdown()
+  }
+
+  // utility methods
+
+  get checkedCheckboxes() {
+    return document.querySelectorAll("div[id=specialties-accordion] input[type=checkbox]:checked");
+  }
+
+  isPillNotDisplayed(domId) {
+    return document.getElementById(`specialty_${domId}`) == null
+  }
+
+  checkCheckbox(domId) {
+    const checkbox = document.getElementById(`specialty_ids_${domId}`);
+    checkbox.checked = true
+  }
+
+  uncheckCheckbox(domId) {
+    const checkbox = document.getElementById(`specialty_ids_${domId}`);
     checkbox.checked = false
   }
 
-  onSpecialityClick(event) {
-    event.preventDefault();
-    const domId = event.currentTarget.dataset.value;
-    this.addPill(domId);
+  clearInputAndDropdown() {
     this.inputTarget.value = "";
     this.dropdownTarget.innerHTML = "";
+  }
+
+  addPill(domId) {
+    if (this.isPillNotDisplayed(domId)) {
+      this.createPill(domId)
+      this.checkCheckbox(domId)
+    }
+  }
+
+  createPill(domId) {
+    const template = document.getElementById(`specialty_${domId}-template`);
+    const content = template.content.cloneNode(true)
+    this.pillContainerTarget.appendChild(content)
+  }
+
+  populatePillsFromCheckedCheckboxes() {
+    this.checkedCheckboxes.forEach((checkedCheckbox) => {
+      this.addPill(checkedCheckbox.value)
+    })
   }
 }
