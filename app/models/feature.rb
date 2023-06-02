df0e29df841203b3017acd28959c2ba57339b03e
rainbowRider5 <mdrzewiecki56@gmail.com>
2023-06-02 15:31:21 +0200

    Refactor Feature class

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,22 +1,19 @@
 class Feature
+  ENABLED_FEATURES = {
+    cancel_subscription: true,
+    badge_filter: !Rails.env.production?,
+    business_welcome_email: true,
+    developer_specialties: !Rails.env.production?,
+    offer_extending: !Rails.env.production?,
+    sort: !Rails.env.production?,
+    redesign: ENV.fetch("REDESIGN", false)
+  }.freeze
+
   def self.enabled?(feature_name)
-    case feature_name.to_sym
-    when :badge_filter
-      !Rails.env.production?
-    when :cancel_subscription
-      true
-    when :redesign
-      ENV.fetch("REDESIGN", false)
-    when :business_welcome_email
-      true
-    when :developer_specialties
-      !Rails.env.production?
-    when :offer_extending
-      !Rails.env.production?
-    when :sort
-      !Rails.env.production?
-    else
-      raise "Unknown feature name: #{feature_name}"
-    end
+    feature_sym = feature_name.to_sym
+
+    raise "Unknown feature name: #{feature_name}" unless ENABLED_FEATURES.include?(feature_sym)
+
+    ENABLED_FEATURES[feature_sym]
   end
 end
