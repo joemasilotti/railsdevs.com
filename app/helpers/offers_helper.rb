4a1165a7e1acafd083b5a2db4dfa7b90de142dcd
rainbowRider5 <mdrzewiecki56@gmail.com>
2023-06-30 13:28:30 +0200

    Fix failing translation

--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -1,7 +1,7 @@
 module OffersHelper
   def offer_state_notice(offer_state)
     case offer_state
-    when "proposed" then t(".proposed_an_offer")
+    when "proposed" then t("offers_helper.offer_state_notice.proposed_an_offer")
     when "accepted" then accepted_offer_notice
     when "declined" then declined_offer_notice
     else
@@ -13,14 +13,14 @@ module OffersHelper
 
   def accepted_offer_notice
     (
-      t(".accepted_the_offer") +
+      t("offers_helper.accepted_offer_notice.accepted_the_offer") +
         inline_svg_tag("icons/solid/check.svg", class: "h-5 w-5 text-green-600", aria_hidden: true)
     ).html_safe
   end
 
   def declined_offer_notice
     (
-      t(".declined_the_offer") +
+      t("offers_helper.declined_offer_notice.declined_the_offer") +
         inline_svg_tag("icons/solid/x_circle.svg", class: "h-5 w-5 text-red-600", aria_hidden: true)
     ).html_safe
   end
