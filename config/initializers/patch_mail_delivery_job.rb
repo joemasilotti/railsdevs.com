e2ce78d297b217c57655a305b989c2b6b24be5d0
Joe Masilotti <joe@masilotti.com>
2025-07-28 12:33:48 -0700

    Fix monkey patch to use correct method

--- config/initializers/patch_mail_delivery_job.rb
+++ config/initializers/patch_mail_delivery_job.rb
@@ -1,5 +1,5 @@
 ActionMailer::MailDeliveryJob.class_eval do
-  def perform(mailer, mail_method, delivery_method, args)
+  def perform_now
     super
   rescue Postmark::InactiveRecipientError => e
     Rails.logger.warn("Postmark inactive recipient: #{e.message}")
