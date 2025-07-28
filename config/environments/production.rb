a79707adeb7fbef920588dc8c53a19a9d97384b5
Joe Masilotti <joe@masilotti.com>
2025-07-28 12:22:42 -0700

    Revert "Rescue from inactive Postmark recipients"
    This reverts commit a26587c680b01059ba82425e21aed5a669e5a56b.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,6 @@ Rails.application.configure do
   }
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_job = "SafeMailerJob"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
